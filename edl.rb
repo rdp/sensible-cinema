7991948c703543796196e24f7500dde708d43130
unknown <rogerpack2005@gmail.com>
2010-06-28 08:24:07 -0600

    add in new thots

---
+++ edl.rb
@@ -0,0 +1,42 @@
+some thoughts in other?
+
+=begin
+roobee.net?
+
+other github
+auto-syncing
+
+
+
+v x
+  mute, coordinate by little buttons to increase by a tenth )
+
+, overlay a black screenv, overlay a black screen, overlay a black screen whenever
+
+an easy editor...
+
+increase volume
+"a mother of controlling the input devices to coerce the movie from one point to another" (drag)
+
+"a mother of controlling the input devices to change volume"
+
+"a methof of applying OCR to the existing film to extrapolate position in the media"
+
+replay certain parts?
+
+overlay with youtube
+overlay with youtube audio
+overlay/replac with specific url of audio/video
+increase volume on the player it self.
+
+overlay alpha transparent pic
+
+grey out the screen...
+
+replay (loop style) certain sections
+drag and release for editing position, where possible (with VLC I guess we have best control).
+
+Programmatically do all of the above, by driving using an external API.
+
+Using audio to auto synchronize across different players, or to re-synchronzie (?)
+
