0a34870dbcc22a13c909c4aa25a086683a8901ff
rogerdpack <rogerpack2005@gmail.com>
2010-07-05 22:01:08 -0600

    more specs

--- lib/overlayer.rb
+++ lib/overlayer.rb
@@ -1,22 +1,54 @@
 require 'sane'
 
+class Time
+  def self.now_f
+    now.to_f
+  end
+end
+
 module OverLayer
   
   def nir(command)
     assert system(File.dirname(__FILE__) + "/../nircmd/nircmd " + command)
   end
   
+  @am_muted = false
+  def am_muted?
+    @am_muted
+  end
+  
   def mute!
-    puts 'muting'
+    @am_muted = true
     nir("mutesysvolume 1")
   end
   
   def unmute!
-    puts 'unmuting'
+    @am_muted = false
     nir("mutesysvolume 0")
   end
   
-  # allow for OverLayer.mute!
+  def overlay sequences, total_time = nil
+    mutes = sequences[:mutes]
+    mutes = mutes.to_a.sort!
+    start_time = Time.now
+    while next_mute = mutes.shift
+      next_mute_absolute_start, next_mute_absolute_end = next_mute
+      cur_time = Time.now - start_time
+      end_mute = start_time + next_mute_absolute_end
+      # we should sleep until the next start...
+      time_till_next_mute = next_mute_absolute_start - cur_time
+      pps 'doing next:', next_mute.inspect, 'with cur', cur_time, Time.now_f, 'end time:', end_mute.to_f, 'time till next:', time_till_next_mute 
+      sleep time_till_next_mute
+      pps 'muting @', Time.now.to_f
+      mute!
+      sleep end_mute - Time.now
+      pps 'unmuting @', Time.now.to_f
+      unmute!
+    end
+    
+  end
+  
+  # allow for OverLayer.mute! et al
   extend self
   
 end
--- spec/spec.overlay.rb
+++ spec/spec.overlay.rb
@@ -3,30 +3,53 @@ require 'spec/autorun'
 require 'sane'
 require_relative '../lib/overlayer'
 
-
 describe OverLayer do
+  
+  before do
+    OverLayer.unmute!
+  end
   
   def play file
     system("sndrec32 /play /close #{file}.wav") # this fails at times?
   end
   
+  def start_good
+    assert !OverLayer.am_muted?
+    play("good")
+  end
+  
+  def start_bad
+    assert OverLayer.am_muted?
+    play("bad")
+  end
+  
   it 'should be able to mute' do
     # you shouldn't hear a beep
+    assert !OverLayer.am_muted?
     OverLayer.mute!
-    play("bad")
+    assert OverLayer.am_muted?
+    start_bad
     sleep 2
     OverLayer.unmute!
+    assert !OverLayer.am_muted?
   end
   
   context 'given you know when to start' do
     
     it 'should mute for 1s' do
-      yaml = [:mutes => {3.0 => 6.0}]
-      play("good") # sec's 0-3
+      yaml = {:mutes => {2.0 => 4.0}}
+      a = Thread.new { OverLayer.overlay yaml, 10 } # 10 seconds
+      pps 'start good', Time.now_f
+      start_good # sec's 0-3
+      pps 'good started:', Time.now_f
       sleep 3
-      play("bad") # sec's 3-6 muted
+      pps 'start bad, time:', Time.now_f
+      #require '_dbg'
+      start_bad # sec's 3-6 muted
       sleep 3
-      play("good") # sec's 6-9 open
+      pps 'good, time:', Time.now_f
+      start_good # sec's 6-9 open
+      a.join
     end
   end
   
@@ -38,3 +61,4 @@ describe OverLayer do
   it 'should allow for real yaml files I guess'
   
 end
+
