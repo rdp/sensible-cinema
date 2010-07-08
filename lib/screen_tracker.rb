05628a4fc21e45e6456438d3799fa8a7f8f72876
rogerdpack <rogerpack2005@gmail.com>
2010-07-07 18:51:33 -0600

    cleanup specs

--- lib/screen_tracker.rb
+++ lib/screen_tracker.rb
@@ -3,10 +3,20 @@ require 'win32/screenshot'
 class ScreenTracker
   def initialize name_or_regex, x,y,x2,y2
     @hwnd = Win32::Screenshot::BitmapMaker.hwnd(name_or_regex) # save us 0.00445136 per time
+    raise 'bad name--perhaps not running yet?' unless @hwnd
     @x = x; @y = y; @x2 = x2; @y2 = y2
+    
+    #max_x1, max_y1, max_x2, max_y2 = Win32::Screenshot::BitmapMaker.dimensions_for(hwnd)
+    
   end
   
   def get_bmp
-    Win32::Screenshot.hwnd_area(@hwnd, @x,@y,@x2,@y2)
+    Win32::Screenshot.hwnd_area(@hwnd, @x,@y,@x2,@y2, 0) {|h,w,bmp| return bmp}
   end
+end
+
+if $0 == __FILE__
+  require 'rubygems'
+  require 'sane'
+  require_relative '../spec/spec.screen_tracker.rb'
 end
--- spec/spec.screen_tracker.rb
+++ spec/spec.screen_tracker.rb
@@ -4,16 +4,28 @@ require_relative '../lib/screen_tracker'
 
 describe ScreenTracker do
   
+  before do
+    @a = ScreenTracker.new("VLC",10,10,20,20)
+  end
+  
   it "should be able to grab a picture from screen coords...probably from the current active window" do
-    a = ScreenTracker.new("VLC",10,10,20,20)
-    a.get_bmp.should_not be_nil    
+    @a.get_bmp.should_not be_nil 
   end
   
-  context "negative numbers should result in an offset always, and work" do
-  
+  it "should raise if unable to find" do
+    proc { ScreenTracker.new("unknown window",10,10,20,20) }.should raise_exception(RuntimeError)
   end
   
-  it "should parse yaml appropro"
+  it "should be fast" do
+    Benchmark.realtime { @a.get_bmp }.should be < 0.3
+  end
+  
+  context "negative numbers should result in an offset always, and work"
+  
+  it "should parse yaml appropro"    
+  
+  after(:all) do
     
+  end
     
 end
