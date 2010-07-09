6cd8615c6a05422e5f9380a2d65eb4ea3e34a0bd
rogerdpack <rogerpack2005@gmail.com>
2010-07-09 16:46:40 -0600

    more specs, except some I think should still be failing...

--- lib/screen_tracker.rb
+++ lib/screen_tracker.rb
@@ -1,18 +1,31 @@
 require 'win32/screenshot'
 
 class ScreenTracker
-  def initialize name_or_regex, x,y,x2,y2
+  def initialize name_or_regex, x,y,width,height
     @hwnd = Win32::Screenshot::BitmapMaker.hwnd(name_or_regex) # save us 0.00445136 per time
     raise 'bad name--perhaps not running yet?' unless @hwnd
-    @x = x; @y = y; @x2 = x2; @y2 = y2
-    
-    #max_x1, max_y1, max_x2, max_y2 = Win32::Screenshot::BitmapMaker.dimensions_for(hwnd)
+    raise 'poor dimentia' if width <= 0 || height <= 0
+    if(x < 0 || y < 0)
+      always_zero, always_zero, max_x, max_y = Win32::Screenshot::BitmapMaker.dimensions_for(@hwnd)
+      if x < 0
+        x = max_x + x
+      end
+      if y < 0
+        y = max_y + y
+      end
+    end
+    @x = x; @y = y; @x2 = x+width; @y2 = y+height
     
   end
   
   def get_bmp
     Win32::Screenshot.hwnd_area(@hwnd, @x,@y,@x2,@y2, 0) {|h,w,bmp| return bmp}
   end
+  
+  def get_relative_coords
+    [@x,@y,@x2,@y2]
+  end
+  
 end
 
 if $0 == __FILE__
--- spec/spec.screen_tracker.rb
+++ spec/spec.screen_tracker.rb
@@ -10,6 +10,7 @@ describe ScreenTracker do
       Win32::Screenshot.window(/VLC/, 0) {}
     rescue
       @pid = IO.popen("C:/program files/VideoLan/VLC/vlc.exe").pid
+      sleep 1
     end
   end
   
@@ -25,11 +26,65 @@ describe ScreenTracker do
     proc { ScreenTracker.new("unknown window",10,10,20,20) }.should raise_exception(RuntimeError)
   end
   
-  it "should be fast" do
+  it "should be at least somewhat fast" do
     Benchmark.realtime { @a.get_bmp }.should be < 0.3
   end
   
-  context "negative numbers should result in an offset always, and work"
+  it "should not allow for negative widths" do
+    proc {ScreenTracker.new("VLC",10,10,-20,20)}.should raise_exception
+  end
+  
+  it "should disallow size 0 widths" do
+    proc {ScreenTracker.new("VLC",10,10,0,0)}.should raise_exception
+  end
+  
+  it "should have different bmp if sizes different" do
+    a = ScreenTracker.new("VLC",10,10,5,5)
+    assert a.get_relative_coords == [10,10,15,15]
+    b = ScreenTracker.new("VLC",10,10,50,50)
+    assert a.get_bmp != b.get_bmp
+  end  
+  
+  context "negative numbers should result in an offset" do
+
+    it "should allow for negative sizes" do
+      a = ScreenTracker.new("VLC",-10,10,5,5)
+      a.get_bmp
+      a = ScreenTracker.new("VLC",-10,-10,10,10) # right to the edge
+      a.get_bmp
+      a = ScreenTracker.new("VLC",10,-10,5,5)
+      a.get_bmp
+    end
+    
+    it "should assign right coords" do
+      a = ScreenTracker.new("VLC",-10,-10,5,5)
+      a.get_bmp
+      x,y,x2,y2=a.get_relative_coords
+      hwnd = Win32::Screenshot::BitmapMaker.hwnd("VLC") 
+      always_zero, always_zero, max_x, max_y = Win32::Screenshot::BitmapMaker.dimensions_for(hwnd)      
+      x.should == max_x-10
+      y.should == max_y-10
+      x2.should == x+5
+      y2.should == y+5
+    end
+    
+    it "should look different with negative than with positive" do
+      a = ScreenTracker.new("VLC",10,10,2,2)
+      b = ScreenTracker.new("VLC",10,10,2,2)
+      c = ScreenTracker.new("VLC",-100,-100,50,50)
+      require 'sane'
+      assert a.get_bmp == b.get_bmp
+      assert c.get_relative_coords != b.get_relative_coords
+      File.binwrite 'c.bmp', c.get_bmp
+      File.binwrite 'b.bmp', b.get_bmp
+      assert c.get_bmp != b.get_bmp
+    end
+    
+    it "should fail with out of bounds sizes" do
+      proc { ScreenTracker.new("VLC",-10,-10,20,20).get_bmp }.should raise_exception
+    end
+    
+  end
   
   it "should parse yaml appropro"    
   
