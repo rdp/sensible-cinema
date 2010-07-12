971f0a17b641afb197ce5450abc1d0ecd3578949
rogerdpack <rogerpack2005@gmail.com>
2010-07-12 08:39:49 -0600

    move

--- spec/convert_image.rb
+++ spec/convert_image.rb
@@ -3,6 +3,6 @@ require 'sane'
 require 'RMagick'
 
     img = Magick::Image.from_blob(File.binread ARGV[0])
-    png = img[0].to_blob {self.format = 'TIFF'}
-    File.open("picture10.tif", "wb") {|file| file.puts png}
+    png = img[0].to_blob {self.format = 'PNM' } # TIFF
+    File.open("picture10.pnm", "wb") {|file| file.puts png}
     
