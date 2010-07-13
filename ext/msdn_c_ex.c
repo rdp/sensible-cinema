062446e30765f4a6e4d1baa4d9334e3ff0a0dbc4
rogerdpack <rogerpack2005@gmail.com>
2010-07-13 16:09:10 -0600

    more failures

--- ext/msdn_c_ex.c
+++ ext/msdn_c_ex.c
@@ -4,7 +4,7 @@ int main(char **args) {
     MMRESULT rc;              // Return code.
     HMIXER hMixer;            // Mixer handle used in mixer API calls.
     MIXERCONTROL mxc;         // Holds the mixer control data.
-    MIXERLINE mxl;            // Holds the mixer line data.
+    MIXERLINE mxl, mline;            // Holds the mixer line data.
     MIXERLINECONTROLS mxlc;   // Obtains the mixer control.
 
     // Open the mixer. This opens the mixer with a deviceID of 0. If you
@@ -14,6 +14,7 @@ int main(char **args) {
     rc = mixerOpen(&hMixer, 0,0,0,0);
     if (MMSYSERR_NOERROR != rc) {
         // Couldn't open the mixer.
+      printf("bad3");
     }
 
     // Initialize MIXERLINE structure.
@@ -23,13 +24,52 @@ int main(char **args) {
     // Specify the line you want to get. You are getting the input line
     // here. If you want to get the output line, you need to use
     // MIXERLINE_COMPONENTTYPE_SRC_WAVEOUT.
-    mxl.dwComponentType = MIXERLINE_COMPONENTTYPE_DST_WAVEIN;
+    // MIXERLINE_COMPONENTTYPE_DST_WAVEIN => fail
+    // appears you can find it more easily thus: http://www.eggheadcafe.com/forumarchives/win32programmerdirectxaudio/Feb2006/post25682865.asp
+    
+    // MIXERLINE_COMPONENTTYPE_DST_SPEAKERS is what AHK uses
+    
+    // MIXERCONTROL_CONTROLTYPE_VOLUME
+    
+    /*
+    
+    MIXERCAPS caps;  
+  
+    MMRESULT error = mixerGetDevCaps((HMIXEROBJ)hMixer, &caps, sizeof(MIXERCAPS));  
+  
+    if (MMSYSERR_NOERROR != error)    
+      return;  
+  
+    for (j = 0; j < caps.cDestinations; j++)  
+  
+    {  
+  
+      mline.dwDestination = j;  
+  
+      error = mixerGetLineInfo((HMIXEROBJ)mixer, &mline, MIXER_GETLINEINFOF_DESTINATION);  
+  
+      if (MMSYSERR_NOERROR == error)  
+  
+      {  
+  
+        if (MIXERLINE_COMPONENTTYPE_DST_WAVEIN == mline.dwComponentType)   {
+          mxl = mline;
+          printf("found it");
+          break;
+        }
+      }
+    
+      } */
+      
+    mxl.dwComponentType = MIXERLINE_COMPONENTTYPE_SRC_WAVEOUT;
 
     rc = mixerGetLineInfo((HMIXEROBJ)hMixer, &mxl,
                            MIXER_GETLINEINFOF_COMPONENTTYPE);
-    if (MMSYSERR_NOERROR == rc) {
+    if (MMSYSERR_NOERROR != rc) {
         // Couldn't get the mixer line.
+      printf("bad4 %d %d %d %d %d %d, %d]", rc, MIXERR_INVALLINE, MMSYSERR_BADDEVICEID, MMSYSERR_INVALFLAG, MMSYSERR_INVALHANDLE, MMSYSERR_INVALPARAM,MMSYSERR_NODRIVER);
     }
+    
 
     // Get the control.
     ZeroMemory(&mxlc, sizeof(mxlc));
@@ -41,10 +81,13 @@ int main(char **args) {
     mxlc.pamxctrl = &mxc;
     ZeroMemory(&mxc, sizeof(mxc));
     mxc.cbStruct = sizeof(mxc);
+    // MIXER_GETLINECONTROLSF_ALL
+    // MIXER_GETLINECONTROLSF_ONEBYTYPE => fail
     rc = mixerGetLineControls((HMIXEROBJ)hMixer,&mxlc,
-                               MIXER_GETLINECONTROLSF_ONEBYTYPE);
+                               MIXER_GETLINECONTROLSF_ALL);
     if (MMSYSERR_NOERROR != rc) {
         // Couldn't get the control.
+      printf("bad2 %d %d %d %d %d %d %d %d]", rc, MIXERR_INVALCONTROL, MIXERR_INVALLINE, MMSYSERR_BADDEVICEID, MMSYSERR_INVALFLAG, MMSYSERR_INVALHANDLE, MMSYSERR_INVALPARAM, MMSYSERR_NODRIVER);
     }
 
     // After successfully getting the peakmeter control, the volume range
@@ -67,7 +110,8 @@ int main(char **args) {
     // of a second or so.
     rc = mixerGetControlDetails((HMIXEROBJ)hMixer, &mxcd,
                                  MIXER_GETCONTROLDETAILSF_VALUE);
-    if (MMSYSERR_NOERROR == rc) {
+    if (MMSYSERR_NOERROR != rc) {
+      printf("bad1");
         // Couldn't get the current volume.
     }
     volume = volStruct.lValue;
