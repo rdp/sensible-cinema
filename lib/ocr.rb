0bb0b0f3864f03822b2ffb398c9d5fb6e31d59eb
rogerdpack <rogerpack2005@gmail.com>
2010-08-03 15:32:58 -0600

    accomodate colons. ughly

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -5,7 +5,15 @@ module OCR
   
   GOCR = File.expand_path(File.dirname(__FILE__) + "/../vendor/gocr048.exe")
   
-  def identify_digit memory_bitmap
+  def identify_digit memory_bitmap, might_be_colon = false
+    if might_be_colon
+      # do processing in-line <sigh>
+      total = (memory_bitmap.scan /\x00{5}+/).length
+      if total >= 3 # really should be 4 for VLC
+        # it had some dots...must have been a colon!
+      end
+      return ":"
+    end
     image = MiniMagick::Image.from_blob(memory_bitmap)
     image.format(:pnm) # expensive, requires convert.exe in path...
     input, output, error, thread_if_on_19 = Open3.popen3 GOCR + " -"
@@ -13,15 +21,13 @@ module OCR
     input.close
     a = output.read
     output.close
-    a
+    a.strip
   end
   
   def version
     `#{GOCR} -h 2>&1`
   end
   
-  
   extend self
-  
   
 end
--- spec/spec.ocr.rb
+++ spec/spec.ocr.rb
@@ -8,8 +8,12 @@ describe OCR do
   end
   
   it "should be able to grab some digits" do
-    OCR.identify_digit(File.binread "images/4.bmp").should == "4"
-    
+    OCR.identify_digit(File.binread "images/4.bmp").should == "4"    
   end
+  
+  it "should be able to grab a colon" do
+    OCR.identify_digit(File.binread("images/colon.bmp"), true).should == ":"
+  end
+  
   
 end
