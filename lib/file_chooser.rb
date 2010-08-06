78fe16c5aac144384f4025edc414689585eed658
rogerdpack <rogerpack2005@gmail.com>
2010-08-06 06:50:42 -0600

    tweak readme, change ordering

--- lib/file_chooser.rb
+++ lib/file_chooser.rb
@@ -1,9 +1,12 @@
 module FileChooser
 
+  # show a popup dialog prompting for them to select a file
+  # pretty ugly
   def choose_file(title, use_this_dir = nil)
     fc = Java::javax::swing::JFileChooser.new("JRuby panel")
     fc.set_dialog_title(title)
     fc.setCurrentDirectory(java.io.File.new(use_this_dir)) if use_this_dir
+    # lodo allow for a FileFilter, too...
     success = fc.show_open_dialog(nil)
     if success == Java::javax::swing::JFileChooser::APPROVE_OPTION
       fc.get_selected_file.get_absolute_path
@@ -11,11 +14,11 @@ module FileChooser
      nil
     end
   end
+  
   extend self
   
 end
 
 if __FILE__ == $0
   p FileChooser.choose_file("test1", '..')
-
 end
