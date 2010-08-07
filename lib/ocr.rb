1b54a5742b20f1181ee13d7e48c0412c775437b0
rogerdpack <rogerpack2005@gmail.com>
2010-08-07 11:57:54 -0600

    pass it around as binary now, I believe anyway

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -7,7 +7,7 @@ require 'open3'
 # helper for OCR'ing single digits that were screen captured
 module OCR
   
-  GOCR = File.expand_path(File.dirname(__FILE__) + "/../vendor/gocr048.exe -C 0-9:/ ")
+  GOCR = File.expand_path(File.dirname(__FILE__) + "/../vendor/gocr048.exe -C 0-9:/S ")
   
   CACHE = {}
   
@@ -15,31 +15,26 @@ module OCR
   def identify_digit memory_bitmap, options = {}
     if CACHE[memory_bitmap]
       return CACHE[memory_bitmap]
-    else
     end
-    might_be_colon = options[:might_be_colon]
-    should_invert = options[:should_invert]
-    if might_be_colon
+    if options[:might_be_colon]
       # do processing in-line <sigh>
       total = (memory_bitmap.scan /\x00{5}+/).length
       if total >= 3 # really should be 4 for VLC
-        # it had some dots...must have been a colon!
+        # it had some darkness...therefore have been a colon!
+        CACHE[memory_bitmap] = ":"
+        return ":"
       end
-      CACHE[memory_bitmap] = ":"
-      return ":"
     end
     image = MiniMagick::Image.from_blob(memory_bitmap)
     image.format(:pnm) # expensive, requires convert.exe in path...
-    if should_invert # mogrify calls it negate...
+    if options[:should_invert] 
+      # mogrify calls it negate...
       image.negate 
     end
     for level in [130, 100] # 100 for hulu...
-      input, output, error, thread_if_on_19 = Open3.popen3 GOCR + " -l #{level} -"
-      input.write image.to_blob
-      input.close
-      a = output.read
-      output.close
+      a = `#{GOCR} -l #{level} #{image.path} 2>NUL`
       a.strip!
+      a = '3' if a == 'S' # sigh
       if a =~ /[0-9]/
         a = a.to_i
         CACHE[memory_bitmap] = a
