87dc2b5f3ebe09414196daa4462a5bbf9d93b9c7
rogerdpack <rogerpack2005@gmail.com>
2010-08-07 09:03:45 -0600

    add failing digit

--- lib/screen_tracker.rb
+++ lib/screen_tracker.rb
@@ -121,7 +121,7 @@ class ScreenTracker
                   @a ||= 1
                   @a += 1
                   p 'unable to identify digit!' + type.to_s + @a.to_s
-                  File.binwrite("bad_digit#{@a}.bmp", digits[type])
+                  File.binwrite("bad_digit#{@a}#{type}.bmp", digits[type])
                 end
                 # early return
                 return
