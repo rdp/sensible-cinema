4955ddbfc2f42399b3436eec00a42a2eeda1d6ab
rogerdpack <rogerpack2005@gmail.com>
2010-08-06 19:09:32 -0600

    use awt now, readme changes

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -1,5 +1,8 @@
 raise 'must be using jruby to be able to run this...at least for now' unless RUBY_PLATFORM =~ /java/
 
+require 'win32/registry'
+
+
 f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w") # create dummy rakefile to indicate success
 f.write("task :default\n")
 f.close
--- lib/file_chooser.rb
+++ lib/file_chooser.rb
@@ -1,18 +1,23 @@
+require 'java'
+
 module FileChooser
 
   # show a popup dialog prompting for them to select a file
   # pretty ugly
   def choose_file(title, use_this_dir = nil)
-    fc = Java::javax::swing::JFileChooser.new("JRuby panel")
-    fc.set_dialog_title(title)
-    fc.setCurrentDirectory(java.io.File.new(use_this_dir)) if use_this_dir
-    # lodo allow for a FileFilter, too...
-    success = fc.show_open_dialog(nil)
-    if success == Java::javax::swing::JFileChooser::APPROVE_OPTION
-      fc.get_selected_file.get_absolute_path
-    else
-     nil
+  
+    fc = java.awt.FileDialog.new(nil, title)
+    if use_this_dir
+      # FileDialog only accepts it a certain way.
+      dir = File.expand_path(use_this_dir).gsub(File::Separator, File::ALT_SEPARATOR)
+      fc.setDirectory(dir) 
     end
+    # lodo allow for a FileFilter, too...
+    fc.show
+    
+    out = fc.get_file
+    fc.remove_notify # allow out app to exit
+    out
   end
   
   extend self
