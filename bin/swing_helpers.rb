a9529b8ef3585b4e7bc15aef1ed3a7f4b2dd33e7
rogerdpack <rogerpack2005@gmail.com>
2010-11-19 05:52:11 -0700

    use wmi now

--- bin/swing_helpers.rb
+++ bin/swing_helpers.rb
@@ -2,38 +2,26 @@ require 'java'
 module SensibleSwing 
  include_package 'javax.swing'
  [JButton, JFrame, JLabel, JPanel, JOptionPane,
-   JFileChooser] # grab these constants (http://jira.codehaus.org/browse/JRUBY-5107)
+   JFileChooser, JComboBox] # grab these constants (http://jira.codehaus.org/browse/JRUBY-5107)
  include_package 'java.awt'
  [FlowLayout, Font]
  include_class 'java.awt.event.ActionListener'
  JFile = java.io.File
  
- class ClickAction
-  
-  include ActionListener
-
-  def initialize &block
-    @block = block
-    raise unless block_given?
-  end
-  
-  def actionPerformed(event)
-    # a click!
-    @block.call
-  end
- end
-
  class JButton
    def initialize *args
     super *args
     set_font Font.new("Tahoma", Font::PLAIN, 11)
    end
+  
    def on_clicked &block
-     # maybe it can only have one on click handler for now?
-     handler = ClickAction.new &block
-     add_action_listener handler
+     raise unless block
+     add_action_listener do |e|
+       block.call
+     end
      self
    end
+  
  end
 
  class JFrame
@@ -41,8 +29,17 @@ module SensibleSwing
      dispose # sigh
    end
   end
+  
+  class JFileChooser
+    # returns nil on failure...
+    def execute
+      success = show_open_dialog nil
+      raise nil unless success == Java::javax::swing::JFileChooser::APPROVE_OPTION
+      get_selected_file.get_absolute_path
+    end
+  end
 
 end
 
-
+# code examples
 # JOptionPane.showInputDialog(nil, "not implemented yet", "not implemented yet", JOptionPane::ERROR_MESSAGE)
