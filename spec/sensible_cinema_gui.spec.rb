24eb69f9031f342c6779e2e0685b8268a9caa34f
rogerdpack <rogerpack2005@gmail.com>
2010-12-06 06:57:29 -0700

    more specz

--- spec/sensible_cinema_gui.spec.rb
+++ spec/sensible_cinema_gui.spec.rb
@@ -3,19 +3,19 @@ load '../bin/sensible-cinema'
 
 module SensibleSwing
   describe MainWindow do
-  
+
     it "should be able to start up" do
       MainWindow.new.dispose# shouldn't crash :)
     end
-  
+
     it "should auto-select a EDL if it matches a DVD's title" do
       MainWindow.new.single_edit_list_matches_dvd("19d121ae8dc40cdd70b57ab7e8c74f76").should_not be nil
     end
-    
+
     it "should not auto-select if you pass it nil" do
       MainWindow.new.single_edit_list_matches_dvd(nil).should be nil
     end
-    
+
     it "should prompt if two EDL's match a DVD title" do
       old_edl = MainWindow::EDL_DIR
       MainWindow.const_set(:EDL_DIR, 'temp')
@@ -29,41 +29,47 @@ module SensibleSwing
       MainWindow.new.single_edit_list_matches_dvd("abcdef1234").should be nil
       MainWindow.const_set(:EDL_DIR, old_edl)
     end
-    
+
     it "should modify path to have mencder available, and ffmpeg, and download them on the fly" do
       ENV['PATH'].should include("mencoder")
     end
 
-   before do
-    @subject = MainWindow.new
-  end
-
-
-class FakeFileChooser
- def set_title x
- end
-def set_file y
-end
-def go
- 'abc'
-end
-
-end
-    it "should be able to do a normal copy to hard drive, edited" do
+    before do
+      @subject = MainWindow.new
       @subject.stub!(:choose_dvd_drive) {
         ["drive", "volume", "19d121ae8dc40cdd70b57ab7e8c74f76"] # happiest baby on the block
       }
       @subject.stub!(:generate_and_run_bat_file) { |*args|
         args[0].should == 'abc'
       }
-     @subject.stub!(:new_filechooser) {
+      @subject.stub!(:new_filechooser) {
         FakeFileChooser.new
-     }
-    @subject.stub!(:get_drive_with_most_space_with_slash) {
-      "e:\\"
       }
-     p @subject.do_copy_dvd_to_hard_drive false
+      @subject.stub!(:get_drive_with_most_space_with_slash) {
+        "e:\\"
+      }
     end
-  
+
+
+    class FakeFileChooser
+      def set_title x
+      end
+      def set_file y
+      end
+      def go
+        'abc'
+      end
+    end
+    
+    it "should be able to do a normal copy to hard drive, edited" do
+      @subject.do_copy_dvd_to_hard_drive(false).should == [false, "abc.fulli.tmp.avi"]
+    end
+    
+    it "should be able to return the full list if it already exists" do
+      FileUtils.touch "abc.fulli.tmp.avi.done"
+      @subject.do_copy_dvd_to_hard_drive(false,true).should == [true, "abc.fulli.tmp.avi"]
+      FileUtils.rm "abc.fulli.tmp.avi.done"
+    end
+
   end
 end
