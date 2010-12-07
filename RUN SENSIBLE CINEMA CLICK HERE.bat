466ec1cc1f945bae44828d2b57b66242441cf3d6
rogerdpack <rogerpack2005@gmail.com>
2010-12-07 07:21:44 -0700

    todo

--- RUN SENSIBLE CINEMA CLICK HERE.bat
+++ RUN SENSIBLE CINEMA CLICK HERE.bat
@@ -2,4 +2,4 @@
 @rem disable any local rubyopt settings...
 @set RUBYOPT=
 @echo This window will display lots of debug message output!
-@cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema || echo you need to install java first!
+@cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema || echo you need to install java first! Please report back the error you see! && pause
