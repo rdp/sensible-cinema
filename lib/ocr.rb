f3a5c7ff0d0457e3ec9d91d6e9d2073a3ff40207
rogerdpack <rogerpack2005@gmail.com>
2010-12-13 04:34:16 -0700

    add copyright everywhere [yeck]

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -1,4 +1,19 @@
-require 'sane' # gem
+Copyright 2010, Roger Pack 
+This file is part of Sensible Cinema.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    require 'sane' # gem
 require 'whichr' # gem
 require_relative 'swing_helpers' # to_filename lodo not have it there
 im_path = File.expand_path(File.dirname(__FILE__) + "/../vendor/cache/imagemagick") # convert.exe wants to only be chosen from here...
