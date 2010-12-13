ef86a33f36cee831e8828b7172c53ea24d3bdf03
rogerdpack <rogerpack2005@gmail.com>
2010-12-13 04:42:31 -0700

    more preamble stuff

--- spec/mplayer_edl.spec.rb
+++ spec/mplayer_edl.spec.rb
@@ -2,18 +2,18 @@
 Copyright 2010, Roger Pack 
 This file is part of Sensible Cinema.
 
-    Foobar is free software: you can redistribute it and/or modify
+    Sensible Cinema is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    Sensible Cinema is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with Sensible Cinema.  If not, see <http://www.gnu.org/licenses/>.
 =end
 require File.dirname(__FILE__) + "/common"
 require_relative '../lib/mplayer_edl'
