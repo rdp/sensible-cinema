e2ed2001371dd41ee4d1a20c3e9b5384eac2fcdb
rogerdpack <rogerpack2005@gmail.com>
2010-12-21 14:06:38 -0700

    less need java messages

--- RUN SENSIBLE CINEMA CLICK HERE.bat
+++ RUN SENSIBLE CINEMA CLICK HERE.bat
@@ -2,5 +2,6 @@
 @rem disable any local rubyopt settings...
 @set RUBYOPT=
 @echo This window will display lots of debug message output!
-@cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema || echo you need to install java first! Please report back the error you see! && pause
+@java -version || echo need to install java first && pause
+@cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema || echo ERROR. Please look for error message, above, and report back the error you see, or fix it && pause
 @rem taskkill /f /im mencoder.exe
--- create or edit sensible cinema decision list files.bat
+++ create or edit sensible cinema decision list files.bat
@@ -2,5 +2,6 @@
 @rem disable any local rubyopt settings...
 @set RUBYOPT=
 @echo This window will display lots of debug message output!
-@cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema --create-mode || echo you need to install java first! please report back the error you see! && pause
+@java -version || echo need to install java first && pause
+@cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema --create-mode || echo ERROR. Please look for error message, above, and report back the error you see, or fix it && pause
 @taskkill /f /im mencoder.exe
