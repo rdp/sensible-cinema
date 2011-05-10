c1de625c7f20011a4d940cea5e4267dea31dc870
rogerdpack <rogerpack2005@gmail.com>
2011-05-10 08:50:16 -0600

    more profs

--- lib/profanity_finder.rb
+++ lib/profanity_finder.rb
@@ -43,14 +43,23 @@
 
 
 profanities = {'hell' => 'heck', 'g' + 
-'o' + 'd' => 'g..', 'lord' => 'lo..', 'damn' => 'da..', 'fu' +
-'ck' => 'fu'}
+'o' + 'd' => 'g..', 'lord' => 'lo..', 'da' + 
+'mn' => 'da..', 'f' + 
+'u' +
+'c' + 'k' => 
+'f...',
+'bi' +
+'tch' => 'b.....',
+'ba' +
+'stard' => 'ba.....'
+}
 
 incoming = File.read(ARGV[0])
 
 for profanity, sanitized in profanities
   # dunno if we should force words to just start with this or contain it anywhere...
   # what about 'godly' for example?
+  # or 'ungodly' ?
   if incoming =~ Regexp.new(profanity, Regexp::IGNORECASE)
     p 'contains', sanitized
   end
