57adefcb0be419f42edf8e217c802ae39e3be2b0
rogerdpack <rogerpack2005@gmail.com>
2011-05-18 09:07:11 -0600

    attempt warn better when shtuff doesn't exist

--- template_bats/RUN SENSIBLE CINEMA CLICK HERE.bat
+++ template_bats/RUN SENSIBLE CINEMA CLICK HERE.bat
@@ -1,9 +1,19 @@
 @echo Welcome to Sensible Cinema...
+@IF NOT EXIST sensible-cinema\VENDOR\CACHE GOTO NOWINDIR
 @echo This window will display lots of debug message output (which you can just ignore)!
 @setlocal
-@rem disable any local rubyopt settings...
+@rem disable any local rubyopt settings, just in case...
 @set RUBYOPT=
-@rem check for java installed...we will need that :)
-@java -version > NUL 2>&1 || echo need to install java JRE first && pause
+@rem check for java installed...we will be needing that :)
+@java -version > NUL 2>&1 || echo need to install java JRE first && pause && GOTO DONE
 @cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema %* || echo ERROR. Please look for error message, above, and report back the error you see, or fix it && pause
 @rem taskkill /f /im mencoder.exe
+GOTO DONE
+
+:NOWINDIR
+@echo "it appears you downloaded sensible cinema straight from github--please download it instead from http://rogerdpack.t28.net/sensible-cinema/"
+@pause
+@start http://rogerdpack.t28.net/sensible-cinema
+GOTO DONE
+
+:DONE
