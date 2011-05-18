eeddce43437c9b4fe8d61d160e11ea92e8f7dcb3
rogerdpack <rogerpack2005@gmail.com>
2011-05-18 17:39:31 -0600

    try to note previously existing work

--- template_bats/RUN SENSIBLE CINEMA CLICK HERE.bat
+++ template_bats/RUN SENSIBLE CINEMA CLICK HERE.bat
@@ -5,7 +5,7 @@
 @rem disable any local rubyopt settings, just in case...
 @set RUBYOPT=
 @rem check for java installed...we will be needing that :)
-@java -version > NUL 2>&1 || echo need to install java JRE first && pause && GOTO DONE
+@java -version > NUL 2>&1 || echo need to install java JRE first please install it from java.com && pause && GOTO DONE
 @cd sensible-cinema && java -cp "./vendor/cache/jruby-complete-1.5.5.jar" org.jruby.Main bin\sensible-cinema %* || echo ERROR. Please look for error message, above, and report back the error you see, or fix it && pause
 @rem taskkill /f /im mencoder.exe
 GOTO DONE
