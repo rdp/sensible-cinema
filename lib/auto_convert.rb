35c861597e3bebb8548f60baa3204b69e74d7fd1
rogerdpack <rogerpack2005@gmail.com>
2011-06-09 18:02:48 -0600

    phew they match

--- lib/auto_convert.rb
+++ lib/auto_convert.rb
@@ -1,4 +1,5 @@
-seconds = (ARGV[0] || 1000).to_f
+seconds = eval(ARGV[0] || "1000").to_f
 p 'syntax: second to convert, first conversion ratio, second conversation ratio'
-p 'converted value is', seconds * (ARGV[1] ||29.97/30).to_f
-p 'another converted value is', seconds* (ARGV[2] ||29.97/30).to_f
+twentyNinePointNineSeven =  30000/1001.0
+p 'converted value is', seconds * (ARGV[1] || twentyNinePointNineSeven/30).to_f
+p 'another converted value is', seconds* (ARGV[2] || 30/twentyNinePointNineSeven).to_f
