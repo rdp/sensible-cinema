bc87ec7a20593ec881a1609819a7e5727033cb7e
rogerdpack <rogerpack2005@gmail.com>
2011-06-11 16:20:06 -0600

    use mac installer script

--- lib/check_installed_mac.rb
+++ lib/check_installed_mac.rb
@@ -3,7 +3,28 @@ require 'os'
 success = true
 
 # MPlayer OSX Extended instead of smplayer... ?
+module CheckInstalledMac
+ def self.check_for_installed name
+  command = {"gocr" => "gocr --help", "convert" => "convert --help", 
+    "mplayer" => "mplayer -version", "ffmpeg" => "ffmpeg -version"}[name]
 
-for string, name in {"gocr --help" => "gocr", "convert --help" => "imagemagick", "mplayer -version" => "mplayer", "ffmpeg -version" => "ffmpeg"}
-  puts 'lacking dependency! Please install ' + name unless system(string + "2>" + OS.dev_null)   
+  raise 'unknown' unless command # double check
+
+  unless system(command + " 1> " + OS.dev_null + " 2>" + OS.dev_null)
+     name = 'ImageMagick' if name == 'convert' # special case...
+     puts 'lacking dependency! Please install ' + name + ' by running $ sudo port install ' + name
+     false
+  else
+    true
+  end
+
+ end
+end
+
+if $0 == __FILE__
+  for name in ['gocr', 'convert', 'mplayer', 'ffmpeg'] do
+    if CheckInstalledMac.check_for_installed name
+      puts 'has dep:' + name
+    end
+  end
 end
