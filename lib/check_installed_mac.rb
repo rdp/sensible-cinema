e4ad68134de33a8f784bed111efa994bee18d9e3
rogerdpack <rogerpack2005@gmail.com>
2011-06-11 16:04:59 -0600

    fix checker commands

--- lib/check_installed_mac.rb
+++ lib/check_installed_mac.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'os'
 success = true
 
-# MPlayer OSX Extended instead of mplayer... ?
+# MPlayer OSX Extended instead of smplayer... ?
 
-for string, name in {"gocr -v" => "gocr", "convert -v" => "imagemagick", "mplayer -v" => "mplayer", "ffmpeg -v" => "ffmpeg"}
+for string, name in {"gocr --help" => "gocr", "convert --help" => "imagemagick", "mplayer -version" => "mplayer", "ffmpeg -version" => "ffmpeg"}
   puts 'lacking dependency! Please install ' + name unless system(string + "2>" + OS.dev_null)   
 end
