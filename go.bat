9039ba03346452bde23f10d7522b516fd1b28045
rogerdpack <rogerpack2005@gmail.com>
2011-06-13 08:03:44 -0600

    ready the cannons

--- go.bat
+++ go.bat
@@ -1,2 +1,2 @@
 @rem NB that you can call it like go --debug
-call j %* bin\sensible-cinema || call jruby %* bin\sensible-cinema
+call j %* bin\sensible-cinema
--- gocreatemode.bat
+++ gocreatemode.bat
@@ -1,1 +1,1 @@
-call j %* bin\sensible-cinema --create-mode || call jruby %* bin\sensible-cinema --create-mode
+call j %* bin\sensible-cinema --create-mode
