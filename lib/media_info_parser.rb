8fd3b3d4ace7c21808c9bd12009e0ba3069a9b71
rogerdpack <rogerpack2005@gmail.com>
2011-06-28 18:40:42 -0600

    got something

--- lib/media_info_parser.rb
+++ lib/media_info_parser.rb
@@ -1,27 +1,26 @@
 module MediaInfoParser
   def self.parse_with_convert_command media_info, filename
     video = audio = nil
-    media_info.split("\n\n").each{|section|
-      section =~ /ID.*:.*(\d+)/
-      id = $1
-      section =~  /Codec ID.*: (\w+)/
-      codec_type = $1
-      if codec_type
-        if section =~ /Video/ && !video
-          raise 'bad video codec:' + codec_type unless codec_type == 'V_MPEG2'
-          section =~ /([\d\.]+) fps/
+    media_info.split("Track ID").each{ |section|
+      section =~ /:(\w+)(\d+)/
+      id = $2
+      section =~  /Stream ID:   (\w+)/ # like V_MPEG-2
+      stream_type = $1
+      if stream_type
+        if section =~ /Frame rate/ && !video
+          section =~ /Frame rate: ([\d\.]+)/
           fps = $1
           raise unless fps
-          raise unless section =~ /Language                         : English/
-          video = "V_MPEG-2, \"#{filename}\", fps=#{fps}, track=#{id}, lang=eng"
-        elsif section =~ /Audio/ && !audio
-          raise unless section =~ /Language                         : English/ # hope english comes first...
+          raise unless section =~ /lang: eng/ # expect...
+          video = "#{stream_type}, \"#{filename}\", fps=#{fps}, track=#{id}, lang=eng"
+        elsif section =~ /Channels:/ && !audio
+          raise unless section =~ /lang: eng/ # hope english audio track comes first...
           # A_AC3, "G:\Video\Sintel_NTSC\title01.mkv", track=2, lang=eng
-          audio = "#{codec_type}, \"#{filename}\", track=#{id}, lang=eng"
+          audio = "#{stream_type}, \"#{filename}\", track=#{id}, lang=eng"
         end
       end
     }
     raise unless video && audio
-    return "--no-pcr-on-video-pid --new-audio-pes --vbr  --vbv-len=500 #{video} #{audio}"
+    return "MUXOPT --no-pcr-on-video-pid --new-audio-pes --vbr  --vbv-len=500\n#{video}\n#{audio}"
   end
 end
