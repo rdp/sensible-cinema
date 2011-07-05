6f7634862cca2e4656220f1e57970b3733fca512
rogerdpack <rogerpack2005@gmail.com>
2011-07-05 15:24:00 -0600

    notes

--- lib/mencoder_wrapper.rb
+++ lib/mencoder_wrapper.rb
@@ -35,6 +35,7 @@ class MencoderWrapper
       #   "This will result in a slightly bigger file, but will not cause problems when demuxing or remuxing into other container formats." LODO no harddup ok ???
       # lodo: can I use ffmpeg to unmux-ify/GOP'ify perhaps?
       # LODO 24000/1001 ?
+      # -vf pullup,softskip for DVD's that mix progressive and something something [some of them]...whatever it even really means :P
       video_opts = "-vf scale=720:480,pullup,softskip,harddup -forceidx -ovc lavc -lavcopts vcodec=mpeg2video:vrc_buf_size=1835:vrc_maxrate=9800:vbitrate=5000:keyint=1:vstrict=0:acodec=ac3:abitrate=192:autoaspect -ofps 30000/1001"
       out += "mencoder \"#{this_file.gsub('"', '\\"')}\" -of mpeg -mpegopts format=dvd:tsaf -alang en -nocache -sid 1000 -oac #{audio_codec} #{video_opts} -o #{@big_temp_fulli} -dvd-device #{this_file} && echo done_grabbing > #{@big_temp_fulli}.done\n"
     end
@@ -86,7 +87,7 @@ class MencoderWrapper
       out += "@rem old DISABLED join way... call mencoder -oac lavc -ovc lavc -of mpeg -mpegopts format=dvd:tsaf -vf scale=720:480,harddup -srate 48000 -af lavcresample=48000 -lavcopts vcodec=mpeg2video:vrc_buf_size=1835:vrc_maxrate=9800:vbitrate=5000:keyint=18:vstrict=0:acodec=ac3:abitrate=192:aspect=16/9 -ofps 30000/1001  #{partials.join(' ')} -o #{to_here_final_file}\n"
       
       delete_prefix = delete_partials ? "" : "@rem "
-      delete_prefix = "" if am_on_developer_machine?
+      delete_prefix = "@rem" if am_on_developer_machine? # for ease of double checking...
 
       out += "#{delete_prefix} del #{@big_temp_fulli}\n"
       out += "#{delete_prefix} del " + partials.join(' ') + "\n"
