7f1bbe4925a07292da46bd688e67adfb54e97f9a
rogerdpack <rogerpack2005@gmail.com>
2011-07-13 11:51:22 -0600

    ref

--- lib/subtitle_profanity_finder.rb
+++ lib/subtitle_profanity_finder.rb
@@ -9,6 +9,10 @@ require_relative 'edl_parser'
 
 module SubtitleProfanityFinder
 
+   # splits into timestamps -> timestamps\ncontent
+   def self.split_to_glops subtitles
+     subtitles.scan(/\d\d:\d\d:\d\d.*?^$/m)
+   end
 
    def self.convert_to_regexps profanity_hash
     all_profanity_combinations = []
@@ -53,7 +57,7 @@ module SubtitleProfanityFinder
 
 
   def self.edl_output incoming_filename, extra_profanity_hash = {}, subtract_from_each_beginning_ts = 0, add_to_end_each_ts = 0
-     incoming = File.read(incoming_filename)
+     subtitles = File.read(incoming_filename)
      raise if subtract_from_each_beginning_ts < 0
      raise if add_to_end_each_ts < 0
 
@@ -122,53 +126,59 @@ module SubtitleProfanityFinder
     }
     bad_profanities.merge! extra_profanity_hash    
 
-   all_profanity_combinations = convert_to_regexps bad_profanities
+    all_profanity_combinationss = [convert_to_regexps bad_profanities]
+    
+    
     output = ''
-    # from a timestamp to a line with nothing :)
-    for glop in incoming.scan(/\d\d:\d\d:\d\d.*?^$/m)
-      for profanity, (sanitized, whole_word) in all_profanity_combinations
-        # dunno if we should force words to just start with this or contain it anywhere...
-        # what about 'g..ly' for example?
-        # or 'un...ly' ? I think we're ok there...
-
-        if glop =~ profanity
-          # create english-ified version
-          # take out timing line, number line
-          sanitized_glop = glop.lines.to_a[1..-1].join(' ')
-          sanitized_glop.gsub!(/[\r\n]/, '') # flatten 3 lines to 1
-          sanitized_glop.gsub!(/<(.|)(\/|)i>/i, '') # kill <i> 
-          sanitized_glop.gsub!(/[^a-zA-Z0-9'""]/, ' ') # kill weird stuff like ellipses
-          sanitized_glop.gsub!(/\W\W+/, ' ') # remove duplicate "  " 's
-          
-          # sanitize
-          for (prof2, (sanitized2, whole_word2)) in all_profanity_combinations
-            if sanitized_glop =~ prof2
-              sanitized_glop.gsub!(prof2, sanitized2)
+    for all_profanity_combinations in all_profanity_combinationss
+      
+      for glop in split_to_glops(subtitles)
+        for profanity, (sanitized, whole_word) in all_profanity_combinations
+          # dunno if we should force words to just start with this or contain it anywhere...
+          # what about 'g..ly' for example?
+          # or 'un...ly' ? I think we're ok there...
+  
+          if glop =~ profanity
+            # create english-ified version
+            # take out timing line, number line
+            sanitized_glop = glop.lines.to_a[1..-1].join(' ')
+            sanitized_glop.gsub!(/[\r\n]/, '') # flatten 3 lines to 1
+            sanitized_glop.gsub!(/<(.|)(\/|)i>/i, '') # kill <i> 
+            sanitized_glop.gsub!(/[^a-zA-Z0-9'""]/, ' ') # kill weird stuff like ellipses
+            sanitized_glop.gsub!(/\W\W+/, ' ') # remove duplicate "  " 's
+            
+            # sanitize
+            for (prof2, (sanitized2, whole_word2)) in all_profanity_combinations
+              if sanitized_glop =~ prof2
+                sanitized_glop.gsub!(prof2, sanitized2)
+              end
             end
+            
+            # because we have duplicate's for the letter l/i, refactor [[[profanity]]]
+            sanitized_glop.gsub!(/\[+/, '[')
+            sanitized_glop.gsub!(/\]+/, ']')
+            
+            # extract timing info
+            timing_line = glop.split("\n").first.strip
+            timing_line =~ /((\d\d:\d\d:\d\d),(\d\d\d) --> (\d\d:\d\d:\d\d),(\d\d\d))/
+            # "00:03:00.0" , "00:04:00.0", "violence", "of some sort",
+            ts_begin = "#{$2}.#{$3}"
+            ts_begin = EdlParser.translate_string_to_seconds ts_begin
+            ts_begin  -= subtract_from_each_beginning_ts
+            ts_begin = EdlParser.translate_time_to_human_readable ts_begin, true
+            ts_end = "#{$4}.#{$5}"
+            ts_end = EdlParser.translate_string_to_seconds ts_end
+            ts_end += add_to_end_each_ts
+            ts_end = EdlParser.translate_time_to_human_readable ts_end, true
+            unless output.contain? ts_begin
+              output += %!"#{ts_begin}" , "#{ts_end}", "profanity", "#{sanitized.gsub(/[\[\]]/, '').strip}", "#{sanitized_glop.strip}",\n!
+            end
+            break
           end
-          
-          # because we have duplicate's for the letter l/i, refactor [[[profanity]]]
-          sanitized_glop.gsub!(/\[+/, '[')
-          sanitized_glop.gsub!(/\]+/, ']')
-          
-          # extract timing info
-          timing_line = glop.split("\n").first.strip
-          timing_line =~ /((\d\d:\d\d:\d\d),(\d\d\d) --> (\d\d:\d\d:\d\d),(\d\d\d))/
-          # "00:03:00.0" , "00:04:00.0", "violence", "of some sort",
-          ts_begin = "#{$2}.#{$3}"
-          ts_begin = EdlParser.translate_string_to_seconds ts_begin
-          ts_begin  -= subtract_from_each_beginning_ts
-          ts_begin = EdlParser.translate_time_to_human_readable ts_begin, true
-          ts_end = "#{$4}.#{$5}"
-          ts_end = EdlParser.translate_string_to_seconds ts_end
-          ts_end += add_to_end_each_ts
-          ts_end = EdlParser.translate_time_to_human_readable ts_end, true
-          output += %!"#{ts_begin}" , "#{ts_end}", "profanity", "#{sanitized.gsub(/[\[\]]/, '').strip}", "#{sanitized_glop.strip}",\n!
-          break
+  
         end
-
+  
       end
-
     end
     output
 
