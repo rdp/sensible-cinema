57e063cd0c95553a1444e2b140404f5faadfadf7
rogerdpack <rogerpack2005@gmail.com>
2011-07-20 21:54:00 -0600

    compat...

--- template_bats/mac/RUN SENSIBLE CINEMA CLICK HERE.sh
+++ template_bats/mac/RUN SENSIBLE CINEMA CLICK HERE.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd ../sensible-cinema
-java -Xdock:name="Sensible Cinema" -Xdock:icon="./vendor/monkey.png" -cp "./vendor/jruby-complete-1.6.2.jar" org.jruby.Main bin/sensible-cinema $1 $2 $3 || (echo ERROR. Please look for error message, above, and report back the error you see, or fix it && read -p "Press any key to continue...")
+java -Xdock:name="Sensible Cinema" -Xdock:icon="./vendor/profs.png" -cp "./vendor/jruby-complete-1.6.2.jar" org.jruby.Main bin/sensible-cinema $1 $2 $3 || (echo ERROR. Please look for error message, above, and report back the error you see, or fix it && read -p "Press any key to continue...")
 
