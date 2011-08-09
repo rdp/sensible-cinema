a63c061ca91d02655571b6392cbf6e7583422a13
rogerdpack <rogerpack2005@gmail.com>
2011-08-09 08:19:35 -0600

    todo

--- upconvert_netflix/latest2/setup_capture_coords.rb
+++ upconvert_netflix/latest2/setup_capture_coords.rb
@@ -15,14 +15,12 @@ screen_tracker = ScreenTracker.new_from_yaml File.binread(player_description), n
 p screen_tracker.get_hwnd_loop_forever
 p screen_tracker.get_coords_of_window_on_display
 x,y,x2,y2 = screen_tracker.get_coords_of_window_on_display
-# all right, except, for youtube, the y2 is too high, includes the red at the bottom 
-# TODO cut off black edges too...why not, eh?
+
+# numbers are all right, except, for youtube "within a user window", when the y2 is too high, and includes the red at the bottom 
 y2 -= 50
-p 'setting to', x, y, x2, y2
 setter = SetupDirectshowFilterParams.new
 setter.set_single_setting 'height', y2 - y
 setter.set_single_setting 'width', x2 - x
 setter.set_single_setting 'start_x', x
 setter.set_single_setting 'start_y', y
-
-#screen_tracker.dump_bmps
+p 'set it to', x, y, x2, y2
