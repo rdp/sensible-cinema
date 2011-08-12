5f09e794ff8baee642321ddf97f69cb277630392
rogerdpack <rogerpack2005@gmail.com>
2011-08-11 22:06:29 -0600

    add demo of non linear edited playback, for instance jumping to a whole new scene or title, or movie or title etc

---
+++ play_with_inserted_scene.bat
@@ -0,0 +1,8 @@
+call vlc dvd://d:\@2 --start-time=10 --stop-time=15 vlc://quit
+call vlc dvd://d:\@1 --start-time=10 --stop-time=15 vlc://quit
+call vlc dvd://d:\@2 --start-time=15 --stop-time=20 vlc://quit
+call vlc dvd://d:\@1 --start-time=10 --stop-time=15 vlc://quit
+call vlc dvd://d:\@2 --start-time=35 --stop-time=40 vlc://quit @rem should have been 20
+call vlc  g:\downloads\big_buck_bunny_480p_h264.mov.crdownload --stop-time=4 vlc://quit
+
+
