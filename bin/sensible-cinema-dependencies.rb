ee9c230847255361362e2189ca5c506a3ec92341
rogerdpack <rogerpack2005@gmail.com>
2011-08-15 10:27:48 -0600

    todo

--- bin/sensible-cinema-dependencies.rb
+++ bin/sensible-cinema-dependencies.rb
@@ -6,7 +6,7 @@ module SensibleSwing
     def self.download full_url, to_here
       require 'open-uri'
       require 'openssl'
-      eval("OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE")
+      eval("OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE") if full_url =~ /https/
       writeOut = open(to_here, "wb")
       writeOut.write(open(full_url).read)
       writeOut.close
