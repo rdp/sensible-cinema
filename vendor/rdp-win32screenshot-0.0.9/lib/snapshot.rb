b52d7a4709ca59f00887a29caee15c5ca251a0d7
rogerdpack <rogerpack2005@gmail.com>
2011-09-23 11:50:51 -0600

    add notes

--- vendor/rdp-win32screenshot-0.0.9/lib/snapshot.rb
+++ vendor/rdp-win32screenshot-0.0.9/lib/snapshot.rb
@@ -6,4 +6,5 @@ p 'taking it'
 #p Win32::Screenshot::BitmapMaker.hwnd(/virtual/i).to_s(16)
 name = ARGV[0] || 'out.bmp'
 Win32::Screenshot::BitmapMaker.capture_area( Win32::Screenshot::BitmapMaker.hwnd(/virtual/i), 0,0,1000,1000) {|h,w,bits| File.binwrite(name, bits)}
+Win32::Screenshot.window( /virtual/i, 0) {|h,w,bits| File.binwrite('1' +name, bits)}
 p name
