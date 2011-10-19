e0fbadf5a4c22e71889535eb6a254d8b1050b891
rogerdpack <rogerpack2005@gmail.com>
2011-10-19 17:42:05 -0600

    move them all around

--- lib/gui/sensible-cinema-create-file.rb
+++ lib/gui/sensible-cinema-create-file.rb
@@ -93,5 +93,134 @@ module SensibleSwing
       generate_and_run_bat_file save_to_edited, edit_list_path, descriptors, file_from, dvd_friendly_name, start_time, end_time, dvd_title_track, should_run_mplayer_after, require_deletion_entry
       [false, fulli] # false means it's running in a background thread :P
     end 
+    
+    def repeat_last_copy_dvd_to_hard_drive
+      generate_and_run_bat_file *LocalStorage['last_params']
+    end
+
+    def get_grabbed_equivalent_filename_once dvd_title, dvd_title_track
+      @_get_grabbed_equivalent_filename_once ||=
+      begin
+        new_existing_file_selector_and_select_file "Please choose the file that is your ripped equivalent of #{dvd_title} (title track #{dvd_title_track}) (.mpg or .ts--see file documentation/how_to_get_files_from_dvd.txt)"
+      end
+    end
+    
+    def get_save_to_filename dvd_title
+      @_get_save_to_filename ||=
+      begin
+        save_to_file_name = dvd_title + ' edited version'
+        save_to_file_name = save_to_file_name.gsub(' ', '_').gsub( /\W/, '') + ".avi" # no punctuation or spaces for now, to not complicate...
+        save_to = new_nonexisting_filechooser_and_go "Pick where to save #{dvd_title} edited version to", nil, get_drive_with_most_space_with_slash + save_to_file_name
+        raise 'no spaces allowed yet' if save_to =~ / /
+        begin
+          a = File.open(File.dirname(save_to) + "/test_file_to_see_if_we_have_permission_to_write_to_this_folder", "w")
+          a.close
+          File.delete a.path
+        rescue Errno::EACCES => e
+          show_blocking_message_dialog "unable to write to that directory, please pick again: " + e.to_s
+          raise 'pick again!'
+        end
+        freespace = get_freespace(save_to)
+        if freespace < 8_000_000_000
+          show_blocking_message_dialog("Warning: there may not be enough space on the disk for #{save_to} 
+          (depending on DVD size, you may need around 10G free--you have #{freespace/1_000_000_000}GB free).  Click OK to continue.")
+        end
+        save_to.gsub(/\.avi$/, '')
+      end
+    end
+    
+    
+    def fulli_dot_done_file_exists? save_to_edited
+      fulli = MencoderWrapper.calculate_fulli_filename save_to_edited
+      File.exist?(fulli + ".done") # stinky!
+    end
+    
+    # to make it stubbable :)
+    def get_mencoder_commands descriptors, file_from, save_to, start_time, end_time, dvd_title_track, require_deletion_entry
+      delete_partials = true unless start_time # in case anybody wants to look really really close [?]
+      MencoderWrapper.get_bat_commands descriptors, file_from, save_to, start_time, end_time, dvd_title_track, delete_partials, require_deletion_entry
+    end
+
+    def generate_and_run_bat_file save_to, edit_list_path, descriptors, file_from, dvd_title, start_time, end_time, dvd_title_track, run_mplayer, require_deletion_entry
+      LocalStorage['last_params'] = [save_to, edit_list_path, descriptors, file_from, dvd_title, start_time, end_time, dvd_title_track, run_mplayer, require_deletion_entry]
+      begin
+        commands = get_mencoder_commands descriptors, file_from, save_to, start_time, end_time, dvd_title_track, require_deletion_entry
+      rescue MencoderWrapper::TimingError => e
+        show_blocking_message_dialog("Appears you chose a time frame with no deletion segment in it--please try again:" + e)
+        return
+      rescue Errno::EACCES => e
+        show_blocking_message_dialog("Appears a file on the system is locked: perhaps you need to close down some instance of mplayer?" + e)
+        return
+      end
+      temp_dir = Dir.tmpdir
+      temp_file = temp_dir + '/vlc.temp.bat'
+      File.write(temp_file, commands)
+      popup_message = <<-EOL
+        Applying #{File.basename edit_list_path} 
+           to #{file_from} (#{dvd_title}).
+        Copying to #{save_to}.
+      EOL
+      if !fulli_dot_done_file_exists?(save_to)
+        popup_message += "This could take quite awhile (several hours), and will prompt you with a chime noise when it is done.\n
+        You can close this window and minimize sensible cinema and continue using your computer while it runs in the background.\n"
+      end
+      
+      if !start_time
+        # assume a full run..
+        popup_message += <<-EOL
+          NB that the created file will be playable only with VLC (possibly also with smplayer), 
+          but probably not with windows media player.
+        EOL
+      end
+      
+      popup = show_non_blocking_message_dialog(popup_message, "OK")
+
+      # allow our popups to still be serviced while it is running
+      @background_thread = Thread.new { run_batch_file_commands_and_use_output_somehow commands, save_to, file_from, run_mplayer }
+      when_thread_done(@background_thread) { popup.dispose }
+      # LODO warn if they will overwrite a file in the end...
+    end
+  
+    def run_batch_file_commands_and_use_output_somehow batch_commands, save_to, file_from, run_mplayer_after_done
+      @buttons.each{|b| b.set_enabled false}
+      success = true
+      lines = batch_commands.lines.to_a
+      total_size = lines.length.to_f
+      @progress_bar.visible=true
+      @progress_bar.set_value(10) # start at 10% always, so they can see something.
+      lines.each_with_index{|line, idx|
+        if success
+          puts "running #{line}"
+          success = system_blocking(line, true)
+          if !success
+            puts "\n", 'line failed: ' + line + "\n" + '   see troubleshooting section in README.txt file! ignoring further processing commands...'
+          end
+        end
+        @progress_bar.set_value(10 + idx/total_size*90)
+      }
+      @progress_bar.visible=false
+      @buttons.each{|b| b.set_enabled true}
+      if success
+        saved_to = save_to + '.avi'
+        if run_mplayer_after_done
+          run_smplayer_non_blocking saved_to, nil, '', false, false, true
+        else
+          if File.exist?(saved_to) && (File.size(saved_to).to_f/File.size(file_from) < 0.5) # less than 50% size is suspicious...indeed...check if exists for unit tests.
+            show_blocking_message_dialog("Warning: file size differs by more than 50%--it's possible that transcoding failed somehow")
+          end            
+          show_in_explorer saved_to
+          PlayAudio.play(File.expand_path(File.dirname(__FILE__)) + "/../vendor/music.wav")
+          msg =  "Done--you may now watch file\n #{saved_to}\n in VLC player (or possibly smplayer)"
+          puts msg # for being able to tell it's done on the command line
+          show_blocking_message_dialog msg
+          show_in_explorer saved_to # and again, just for kicks [?]
+        end
+      else
+        SwingHelpers.show_blocking_message_dialog "Failed--please examine console output and report back!\nAlso consult the documentation/troubleshooting file.", "Failed", JOptionPane::ERROR_MESSAGE
+      end
+    end
+    
+    
+    
   end
 end
