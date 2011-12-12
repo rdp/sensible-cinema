83a798f23778ae4dfeacfbd0154a21525e2429c6
rogerdpack <rogerpack2005@gmail.com>
2011-12-12 10:35:59 -0700

    license

--- lib/gui/sensible-cinema-dependencies.rb
+++ lib/gui/sensible-cinema-dependencies.rb
@@ -131,7 +131,7 @@ module SensibleSwing
   
     def require_blocking_license_accept_dialog program, license_name, license_url_should_also_be_embedded_by_you_in_message, 
       title = 'Confirm Acceptance of License Agreement', message = nil
-      puts 'Please confirm license agreement in open window.'
+      puts 'Please confirm license agreement in open window before proceeding.'
       
       message ||= "Sensible Cinema requires a separately installed program (#{program}), not yet installed.
         You can install this program manually to the vendor/cache subdirectory, or Sensible Cinema can download it for you.
@@ -140,9 +140,9 @@ module SensibleSwing
         Click 'View License' to view it.  If you do not agree to these terms, click 'Cancel'.  You also agree that this is a 
         separate program, with its own distribution, license, ownership and copyright.  
         You agree that you are responsible for the download and use of this program, within sensible cinema or otherwise."
-      answer = JOptionPane.show_select_buttons_prompt message, :no => "I Accept the Terms of the #{license_name} License Agreement.", :yes => "View #{license_name}"
+      answer = JOptionPane.show_select_buttons_prompt message, :no => "I Have read and Accept the Terms of the #{license_name} License Agreement.", :yes => "View #{license_name}"
       assert_confirmed_dialog answer, license_url_should_also_be_embedded_by_you_in_message
-      p 'confirmation of sensible cinema related license noted of: ' + license_name # LODO require all licenses together :P
+      p 'confirmation of sensible cinema related license duly noted of: ' + license_name # LODO require all licenses together :P
     end
     
     def assert_confirmed_dialog returned, license_url_should_also_be_embedded_by_you_in_message
