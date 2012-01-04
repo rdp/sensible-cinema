6a4554b24b54b83dfc616f7c0827cc9ecff5cfa4
rogerdpack <rogerpack2005@gmail.com>
2012-01-04 14:09:03 -0700

    ref

--- www/youtube_edl/render_edited.rb
+++ www/youtube_edl/render_edited.rb
@@ -1,14 +1,23 @@
 require 'erb'
 $template = ERB.new File.read("control_youtube.rhtml")
 
-def render_edited out, incoming_params
-    mutes = []
-    incoming_params['mute_starts'].each_with_index{|start, idx|
+def combine_arrays array1, array2
+    array1 ||= []
+    array2 ||= []
+    raise unless array1.length == array2.length
+    out = []
+    array1.each_with_index{|start, idx|
       start = start.to_f
-      endy = incoming_params['mute_ends'][idx].to_f
-      mutes << "[#{start},#{endy}]"
-      out.puts "bad #{endy} < #{start}"  unless endy > start
+      endy = array2[idx].to_f
+      out << "[#{start},#{endy}]"
+      raise "bad #{endy} < #{start}"  unless endy > start
     }
+    out
+end
+
+
+def render_edited out, incoming_params
+    mutes = combine_arrays incoming_params['mute_starts'], incoming_params['mute_ends']
     out.puts 'mutes:' + mutes.join(',')
     out.puts $template.result(binding)
 end
