73f1996c44d90a6542812883b1eecedecc2967a8
rogerdpack <rogerpack2005@gmail.com>
2012-01-06 10:07:54 -0700

    update www for release <gulp>

--- www/youtube_edl/render_edited.rb
+++ www/youtube_edl/render_edited.rb
@@ -15,13 +15,11 @@ def combine_arrays array1, array2
     out
 end
 
-
 def render_edited out, incoming_params
     mutes = combine_arrays incoming_params['mute_start'], incoming_params['mute_end']
     splits = combine_arrays incoming_params['skip_start'], incoming_params['skip_end']
     video_id =  incoming_params['youtube_video_id'][0]
     out.puts $template.result(binding)
    # html tag has already been closed...
-    out.puts 'mutes:' + mutes.join(',') + ' skips:' + splits.join(',') + "\n"
-    out.puts "demo: http://rogerdpack.t28.net/sensible-cinema/youtube_edl/yo?mute_start=2&mute_end=7&skip_start=10&skip_end=20&youtube_video_id=ylLzyHk54Z0"
+    out.puts 'mutes: ' + mutes.join(', ') + ' skips: ' + splits.join(', ') + "\n"
 end
