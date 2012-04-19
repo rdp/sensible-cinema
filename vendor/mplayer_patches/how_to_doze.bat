b8fdd9b21bcd118716b29ac0903ce6e0070a7df1
rogerdpack <rogerpack2005@gmail.com>
2012-04-18 18:51:59 -0600

    win32 up2date

---
+++ vendor/mplayer_patches/how_to_doze.bat
@@ -0,0 +1,5 @@
+git clone https://rdp@github.com/rdp/mplayer-svn.git # with submodules :P
+
+@rem  --enable-iconv --enable-freetype --enable-static --enable-fontconfig --enable-runtime-cpudetection --enable-debug
+
+then edit config.mak -lexpat -lfontconfig
