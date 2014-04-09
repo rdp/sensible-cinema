f686f2130d79f98f11c38bd6cd38b0b3ebe569ee
rogerdpack <rogerpack2005@gmail.com>
2014-04-09 15:23:05 -0600

    use its built in template system, I guess like rhtml

--- goweb/wiki.go
+++ goweb/wiki.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
         "net/http" // std lib
+	"html/template"
 )
 
 type Page struct {
@@ -48,12 +49,8 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         p = &Page{Title: title}
     }
-    fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-        "<form action=\"/save/%s\" method=\"POST\">"+
-        "<textarea name=\"body\">%s</textarea><br>"+
-        "<input type=\"submit\" value=\"Save\">"+
-        "</form>",
-        p.Title, p.Title, p.Body)
+    t, _ := template.ParseFiles("edit.html")
+    t.Execute(w, p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
