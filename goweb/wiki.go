4a83630095f97c9b537dd2a4995bfa9ccebe21ac
rogerdpack <rogerpack2005@gmail.com>
2014-04-10 13:50:23 -0600

    woot, though not quite as coolio as theirs, may be functional...

--- goweb/wiki.go
+++ goweb/wiki.go
@@ -85,7 +85,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
         m := validPath.FindStringSubmatch(r.URL.Path)
         if m == nil {
             http.NotFound(w, r)
-            return
+            return 
         }
         fn(w, r, m[2])
     }
