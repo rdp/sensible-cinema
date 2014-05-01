d4c40b7ae60889471cf5d2599d32ee967dd5218c
rogerdpack <rogerpack2005@gmail.com>
2014-05-01 12:41:52 -0600

    got as far as the mouse, woot

--- lib/gui/online-player.rb
+++ lib/gui/online-player.rb
@@ -2,20 +2,21 @@
 module SensibleSwing
   
   class MainWindow
-
     def setup_online_player_buttons
-      require_relative '../online_movie_players.rb'
-	  
+      require_relative '../online_movie_players.rb'	 
 	  add_text_line 'Online Player playback Options:'
       new_jbutton("Attempt to auto detect player and select url to use") do
-        go_sc
+        go_online
       end
             
-      new_jbutton("Open Website for editing/creating EDL's") do
-         SimpleGuiCreator.open_url_to_view_it_non_blocking "http://inet2.org:8080/"
+      new_jbutton("Open Website for viewing/editing movie edits") do
+         SimpleGuiCreator.open_url_to_view_it_non_blocking "http://198.199.93.93/"
       end
 	  # not pertinent enough...
 	  # add_open_documentation_button
+	  if ARGV.contain?('--go')
+	    go_online
+	  end
     end
   end
 end
