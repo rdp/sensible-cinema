bc49c40920b75d1f2cd941d6fec3e77189bb5b6e
rogerdpack <rogerpack2005@gmail.com>
2016-09-28 16:01:13 -0600

    moh

--- html5_javascript/kemal_server/src/kemal_server.cr
+++ html5_javascript/kemal_server/src/kemal_server.cr
@@ -6,6 +6,16 @@ end
 
 require "kemal"
 
+
+before_all "/" do |env|
+  puts "Setting response content type"
+  env.response.content_type = "application/json"
+end
+
+db = ConnectionPool.new(capacity: 25, timeout: 0.01) do
+  DB.open(ENV["DATABASE_URL"])
+end
+
 get "/" do
   "Hello World!"
 end
