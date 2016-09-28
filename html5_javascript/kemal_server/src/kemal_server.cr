5c5d4df32ac6704c9cef240f4b7b2ecfb06d9c07
rogerdpack <rogerpack2005@gmail.com>
2016-09-28 16:08:34 -0600

    wow it actually worked what

--- html5_javascript/kemal_server/src/kemal_server.cr
+++ html5_javascript/kemal_server/src/kemal_server.cr
@@ -1,23 +1,18 @@
 require "./kemal_server/*"
 
-module KemalServer
+# module KemalServer
   # TODO Put your code here
-end
+# end
 
 require "kemal"
 
-
-before_all "/" do |env|
-  puts "Setting response content type"
-  env.response.content_type = "application/json"
-end
-
-db = ConnectionPool.new(capacity: 25, timeout: 0.01) do
-  DB.open(ENV["DATABASE_URL"])
-end
-
 get "/" do
-  "Hello World!"
+  "Hello World! <a href=for_current>scriptz</a>"
+end
+
+get "/for_current" do |env|
+  env.response.content_type = "application/javascript"
+  render "src/views/html5_edited.js.ecr"
 end
 
 Kemal.run
