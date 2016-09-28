d5bb353f42ef433e678f32b9f86de7c3efccb207
rogerdpack <rogerpack2005@gmail.com>
2016-09-28 16:29:09 -0600

    it can serve file based LOL

--- html5_javascript/kemal_server/src/kemal_server.cr
+++ html5_javascript/kemal_server/src/kemal_server.cr
@@ -7,16 +7,23 @@ require "./kemal_server/*"
 require "kemal"
 
 get "/" do
-  "Hello World! <a href=for_current>scriptz</a>"
+  "Hello World! <a href=for_current?url_escaped=https%3A%2F%2Fwww.netflix.com%2Fwatch%2F80016224>scriptz</a>"
 end
 
 get "/for_current" do |env|
 
-  url = env.params.query["url_no_slashes"]
-  log("hello #{url}")
-
-  env.response.content_type = "application/javascript"
-  render "src/views/html5_edited.js.ecr"
+  url_unescaped = env.params.query["url_escaped"] # sad but true
+  url_escaped = URI.escape url_unescaped
+  log("hello #{url_escaped}")
+  path = "edit_descriptors/#{url_escaped}" 
+  if (!File.exists?(path) || url_escaped.includes?(".."))
+    env.response.status_code = 403
+    "unable to find one yet for #{path}" # never did figure out how to write this to the output :|
+  else
+    mutes_and_skips = File.read path
+    env.response.content_type = "application/javascript"
+    render "src/views/html5_edited.js.ecr"
+  end
 end
 
 Kemal.run
