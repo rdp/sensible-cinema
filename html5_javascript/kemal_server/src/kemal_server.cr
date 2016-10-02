a27f5686a630ce159db986ec44cdb57be63cefb9
rogerdpack <rogerpack2005@gmail.com>
2016-10-01 19:11:30 -0600

    and it was a bad template LOL

--- html5_javascript/kemal_server/src/kemal_server.cr
+++ html5_javascript/kemal_server/src/kemal_server.cr
@@ -62,7 +62,7 @@ get "/edit" do |env| # same as "view" :)
     current_text = "// template [DELETE THIS LINE]:
 var name=\"#{title}\";
 var fast_forwards=[[50.0, 51.0]];
-var mutes=[[2.0,7.0]]; 
+var mutes=[[2.0,7.0]];
 var skips=[[10.0, 30.0]];"
   end
   
