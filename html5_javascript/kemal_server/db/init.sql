0ccaaeeef065364ca315e9e00dd772ca1cc89f0e
rogerdpack <rogerpack2005@gmail.com>
2016-10-03 15:32:32 -0600

    moh db

--- html5_javascript/kemal_server/db/init.sql
+++ html5_javascript/kemal_server/db/init.sql
@@ -1,16 +1,19 @@
+--run with recreate.sh
 CREATE TABLE URL(
    ID INT PRIMARY KEY     NOT NULL,
    URL           TEXT    NOT NULL,
-   name          TEXT     NOT NULL, // CHAR(50)
+   name          TEXT     NOT NULL, -- CHAR(50)
    SALARY         REAL
 );
+create index url_url_index on URL(URL); 
 
 CREATE TABLE EDITS (
    ID INT PRIMARY KEY     NOT NULL,
    START          REAL,
    END            REAL,
-   CATEGORY       TEXT NOT NULL,
-   DETAILS        TEXT NOT NULL,
-   URL_ID         INT,
-   FOREIGN KEY(URL_ID) REFERENCES URL(ID)
+   CATEGORY       TEXT NOT NULL, -- deity or violence
+   SUBCATEGORY    TEXT NOT NULL, -- gore-3
+   DETAILS        TEXT NOT NULL, -- gosh what is going on? LOL
+   URL_ID         INT, FOREIGN KEY(URL_ID) REFERENCES URL(ID)
 );
+
---
+++ html5_javascript/kemal_server/db/recreate.sh
@@ -0,0 +1,2 @@
+rm ./db/sqlite3_data.db
+sqlite3 ./db/sqlite3_data.db < ./db/init.sql
