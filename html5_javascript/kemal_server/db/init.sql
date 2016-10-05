6dc3bcd223fd1f43343c07a93d1c765b1964a644
rogerdpack <rogerpack2005@gmail.com>
2016-10-05 14:31:12 -0600

    move elsewhere

--- html5_javascript/kemal_server/db/init.sql
+++ html5_javascript/kemal_server/db/init.sql
@@ -23,6 +23,14 @@ insert into urls (url, name) values ("http://url", 'a_name";alert("xss");"'); --
 insert into edits (start, endy, category, subcategory, subcategory_level, details, default_action, url_id) values
     (20090.50, 20100.50, "a category", "a subcat", 3, "details", "mute", last_insert_rowid());
 -- output some to screen
+
+insert into urls (url, name) values ("https://www.netflix.com/watch/80016224", 'meet the mormons [test]');
+insert into edits (start, endy, category, subcategory, subcategory_level, details, default_action, url_id) values
+      (2.0, 7.0, "a category", "a subcat", 3, "details", "skip", (select id from urls where url='https://www.netflix.com/watch/80016224'));
+insert into edits (start, endy, category, subcategory, subcategory_level, details, default_action, url_id) values
+          (10.0, 30.0, "a category", "a subcat", 3, "details", "mute", (select id from urls where url='https://www.netflix.com/watch/80016224'));
+
+
 select * from urls;
 select * from edits;
 
