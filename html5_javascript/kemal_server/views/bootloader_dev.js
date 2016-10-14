3e8f62e1266efdc82ebb7c51d3f636a59f21b836
rogerdpack <rogerpack2005@gmail.com>
2016-10-14 14:44:17 -0400

    closer to reload anyway

--- html5_javascript/kemal_server/views/bootloader_dev.js
+++ html5_javascript/kemal_server/views/bootloader_dev.js
@@ -1,8 +1,15 @@
-current_url_sanitized = window.location.href.split("?")[0];
-current_url_sanitized = current_url_sanitized.replace("smile.amazon", "www.amazon");
-if ( current_url_sanitized.includes("/dp/") ) {
-  id = current_url_sanitized.split("/dp/")[1].split("/")[0]
-  current_url_sanitized = "https://www.amazon.com/gp/product/" + id
+
+function getSanitizedCurrentUrl() {
+  current_url = window.location.href;
+  # and sanitize
+  if (!current_url.contain("play.google.com"))
+    current_url = current_url.split("?")[0];
+  current_url = current_url.replace("smile.amazon", "www.amazon");
+  if (current_url.includes("/dp/") ) {
+    id = current_url.split("/dp/")[1].split("/")[0]
+    current_url = "https://www.amazon.com/gp/product/" + id
+  }
+  return current_url;
 }
 
 function getCurrentAmazonEpisode() {
@@ -15,7 +22,7 @@ function getCurrentAmazonEpisode() {
 
 loaded=false;
 
-javascript:(function(e,s){e.src=s;e.onload=function(){loaded=true;};document.head.appendChild(e);})(document.createElement('script'),'//rawgit.com/rdp/sensible-cinema-edit-descriptors/master/' + encodeURIComponent (encodeURIComponent(current_url_sanitized + getCurrentAmazonEpisode() + ".rendered.js")));
+javascript:(function(e,s){e.src=s;e.onload=function(){loaded=true;};document.head.appendChild(e);})(document.createElement('script'),'//rawgit.com/rdp/sensible-cinema-edit-descriptors/master/' + encodeURIComponent (encodeURIComponent(getSanitizedCurrentUrl() + getCurrentAmazonEpisode() + ".rendered.js")));
 <!-- // double encode needed apparently :| jquery hopefully already loaded on every site?? hrm... -->
 
 setTimeout(function(){ if (loaded == false) alert("unable to load for your current movie"); }, 3000);
