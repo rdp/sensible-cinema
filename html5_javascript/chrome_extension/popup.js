9e44d05b47be599984ade4750bd6530ad2e76a61
rogerdpack <rogerpack2005@gmail.com>
2016-10-18 15:37:00 -0600

    it's a miracle something worked at all

--- html5_javascript/chrome_extension/popup.js
+++ html5_javascript/chrome_extension/popup.js
@@ -2,37 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-function findFirstVideoTag(node) {
-    // there's probably a jquery way to do this easier :)
-    if (node.nodeType == 1) {
-        if (node.tagName.toUpperCase() == 'VIDEO') { // assume html 5 <VIDEO  ...
-            return node;
-        }
-        node = node.firstChild;
-
-        while (node) {
-            if ((out = findFirstVideoTag(node)) != null) {
-                return out;
-            }
-            node = node.nextSibling;
-        }
-    }
-}
-
-function loadEditedPlayback() {
-javascript:(function(e,s){e.src=s;e.onload=function(){;;;;;};document.head.appendChild(e);})(document.createElement('script'),'//rawgit.com/rdp/sensible-cinema/master/html5_javascript/kemal_server/views/bootloader_dev.js');
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   // only enters here after they click on the icon
-  video_element = findFirstVideoTag(document.body);
   var h=document.getElementById("edited_requested");
-  h.addEventListener("click", loadEditedPlayback());
-  alert('did something' + h + " " + video_element + " \n" + '');
+  //h.addEventListener("click", loadEditedPlayback());
 (function(d, script) {
     script = d.createElement('script');
     script.type = 'text/javascript';
@@ -40,12 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
     script.onload = function(){
         // remote script has loaded
     };
-    script.src = 'https://rawgit.com/rdp/sensible-cinema/master/html5_javascript/kemal_server/views/bootloader_dev.js'; // it still rejects it
+    script.text = 'alert("we are here");'
     d.getElementsByTagName('head')[0].appendChild(script);
 }(document));
 });
-
-function loadEditedPlayback() {
-javascript:(function(e,s){e.src=s;e.onload=function(){;;;;;};document.head.appendChild(e);})(document.createElement('script'),'//rawgit.com/rdp/sensible-cinema/master/html5_javascript/kemal_server/views/bootloader_dev.js');
-}
 
