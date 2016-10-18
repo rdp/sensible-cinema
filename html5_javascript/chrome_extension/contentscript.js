aac981260161ac35f9992e103d90fda5ce747ef5
rogerdpack <rogerpack2005@gmail.com>
2016-10-18 16:41:28 -0600

    hallelujah LOL

--- html5_javascript/chrome_extension/contentscript.js
+++ html5_javascript/chrome_extension/contentscript.js
@@ -1,9 +1,11 @@
+
 function injectJs(link) {
-var scr = document.createElement('script');
-scr.type="text/javascript";
-scr.src=link;
-document.getElementsByTagName('head')[0].appendChild(scr)
-//document.body.appendChild(scr);
+  var scr = document.createElement('script');
+  scr.type="text/javascript";
+  scr.src=link;
+  // scr.text = 'alert("we are here 2");'
+  document.getElementsByTagName('head')[0].appendChild(scr)
+  //document.body.appendChild(scr);
 }
 
 injectJs(chrome.extension.getURL('injected.js'));
