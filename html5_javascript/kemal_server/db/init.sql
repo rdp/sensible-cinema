214dbbd7f3a27ec36af1bbdb80ff7c1329d463af
rogerdpack <rogerpack2005@gmail.com>
2016-10-27 12:38:08 -0600

    todo

--- html5_javascript/kemal_server/db/init.sql
+++ html5_javascript/kemal_server/db/init.sql
@@ -46,7 +46,7 @@ alter table urls CHANGE editing_notes editing_status VARCHAR(1024);
 --
 
 alter table urls ADD COLUMN image_url VARCHAR(2014) NOT NULL DEFAULT '';
-update urls set image_url = 'https://upload.wikimedia.org/wikipedia/en/b/ba/Airbender-CompleteBook3.jpg';
+update urls set image_url = 'https://upload.wikimedia.org/wikipedia/en/b/ba/Airbender-CompleteBook3.jpg' where id = 1;;
 alter table urls ADD COLUMN is_amazon_prime INT NOT NULL DEFAULT 0; -- probably should be TINYINT(1) but crystal mysql adapter no support it [?]
 alter table urls ADD COLUMN rental_cost DECIMAL NOT NULL DEFAULT 0.0; -- too scared to use floats
 alter table urls ADD COLUMN purchase_cost DECIMAL NOT NULL DEFAULT 0.0;
