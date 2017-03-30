1fca069c80da88b3d49c8579f2b00dbaa8b0c660
rogerdpack <rogerpack2005@gmail.com>
2017-03-30 14:59:23 -0600

    todos

--- html5_javascript/kemal_server/sync_to_prod_with_commit.sh
+++ html5_javascript/kemal_server/sync_to_prod_with_commit.sh
@@ -1,1 +1,2 @@
-git pull && git cam "$1" && git pom && curl https://playitmyway.inet2.org/sync_web_server
+git pull && git cam "$1"  # this one can fail if none
+git pom && curl https://playitmyway.inet2.org/sync_web_server
