7915977a6249939eb3b79137d6e8196101c99a53
rogerdpack <rogerpack2005@gmail.com>
2017-04-01 17:40:04 -0700

    yup

--- html5_javascript/kemal_server/src/kemal_server.cr
+++ html5_javascript/kemal_server/src/kemal_server.cr
@@ -235,7 +235,7 @@ post "/save_tag/:url_id" do |env|
   }
   save_local_javascript [url], tag.inspect, env
   if tag.duration > 10*60
-    add_to_flash("warning, duration is > 10 minutes of tag just saved??")
+    add_to_flash(env, "warning, duration is > 10 minutes of tag just saved??")
   end
   add_to_flash(env, "saved tag's details, you can close this window now, hit reload in your browser...")
   env.redirect "/edit_tag/#{tag.id}"
