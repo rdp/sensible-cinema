54cf54724e89456852222768609006942ada7755
rogerdpack <rogerpack2005@gmail.com>
2017-04-06 19:52:41 -0600

    huh

--- html5_javascript/chrome_extension/popup.js
+++ html5_javascript/chrome_extension/popup.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 document.addEventListener('DOMContentLoaded', function() {
   // only enters here after they click on the icon
   // no access *at all* to the current tab DOM apparently :|
@@ -24,9 +20,6 @@ function openIndex() {
   // this is how you have to do any links from the popup box [yikeserz]
   chrome.tabs.create({active: true, url: "https://playitmyway.inet2.org"});
 }
-
-// XXXX somehow get status or request status from content script at load time :|
-// until then, guess the "..." will have to do...
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.browserAction.getTitle({ tabId: tabs[0].id }, function(title) {
