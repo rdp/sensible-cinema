02c77306bcdc001a71816191e30f5a7083f537cf
rogerdpack <rogerpack2005@gmail.com>
2017-04-12 14:10:03 -0600

    cleanup

--- html5_javascript/kemal_server/src/kemal_server.cr
+++ html5_javascript/kemal_server/src/kemal_server.cr
@@ -4,6 +4,7 @@ require "kemal"
 require "kemal-session"
 require "http/client"
 require "mysql"
+require "./view_helpers" # make accessible to all views
 
 Session.config do |config|
   config.secret = File.read("./db/local_cookie_secret") # generate like crystal eval 'require "secure_random"; puts SecureRandom.hex(64)' > local_cookie_secret
@@ -663,8 +664,6 @@ post "/upload_from_subtitles_post/:url_id" do |env|
   env.redirect "/view_url/" + db_url.id.to_s
 end
 
-####### view methods :)
-
 def add_to_flash(env, string)
   if env.session.string?("flash")
     env.session.string("flash", env.session.string("flash") + "<br/>" + string)
@@ -673,29 +672,5 @@ def add_to_flash(env, string)
   end
 end
 
-def if_present_with_break(value, add_this_before)
-  if value.to_s.size > 0 && value != 0
-    add_this_before + " " + value.to_s + "<br/>"
-  end
-end
-
-def table_row_or_nothing(first_cell, second_cell)
-  if second_cell.to_s.size > 0 && second_cell != 0
-    "<tr><td>#{first_cell}</td><td>#{second_cell}</td></tr>";
-  end
-end
-
-def google_search_string(url)
-   google_search = URI.escape(url.name, true)
-   if url.episode_number != 0
-     google_search += URI.escape(" " + url.episode_number.to_s + " " + url.episode_name, true)
-   end
-	 google_search
-end
-
-def is_mobile(env)
-  env.request.headers["User-Agent"] =~ /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/
-end
-
 Kemal.run
-sleep # needed for my cruddy sync stuff?
+sleep # needed for my cruddy restart after sync stuff?? :|
---
+++ html5_javascript/kemal_server/src/view_helpers.cr
@@ -0,0 +1,24 @@
+
+def if_present_with_break(value, add_this_before = "")
+  if value.to_s.size > 0 && value != 0
+    add_this_before + " " + value.to_s + "<br/>"
+  end
+end
+
+def table_row_or_nothing(first_cell, second_cell)
+  if second_cell.to_s.size > 0 && second_cell != 0
+    "<tr><td>#{first_cell}</td><td>#{second_cell}</td></tr>";
+  end
+end
+
+def google_search_string(url)
+   google_search = URI.escape(url.name, true)
+   if url.episode_number != 0
+     google_search += URI.escape(" " + url.episode_number.to_s + " " + url.episode_name, true)
+   end
+   google_search
+end
+
+def is_mobile(env)
+  env.request.headers["User-Agent"] =~ /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/
+end
