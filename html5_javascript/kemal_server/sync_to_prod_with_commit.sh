e02e59e9a1fd6b27f94593fc57dbed2c43efa475
rogerdpack <rogerpack2005@gmail.com>
2017-04-13 15:03:01 -0600

    comments

--- html5_javascript/kemal_server/sync_to_prod_with_commit.sh
+++ html5_javascript/kemal_server/sync_to_prod_with_commit.sh
@@ -1,7 +1,7 @@
-git pull && git cam "$1"  # this one can fail if none
+git pull && git cam "$1"  # this one can fail if none, that's OK
 git pom && curl https://playitmyway.inet2.org/sync_web_server
 until $(curl --output /dev/null --silent --head --fail https://playitmyway.inet2.org); do
     printf '.'
     sleep 1
 done
-echo
+echo "server is back up and running again"
