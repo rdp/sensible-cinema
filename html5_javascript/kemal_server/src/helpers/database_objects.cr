e63a05224f026fa6d0fa501f58c2eaeaa3b7f6fd
rogerdpack <rogerpack2005@gmail.com>
2017-04-17 17:55:19 -0600

    m

--- html5_javascript/kemal_server/src/helpers/database_objects.cr
+++ html5_javascript/kemal_server/src/helpers/database_objects.cr
@@ -446,7 +446,7 @@ class Tag
   end
 
   def duration
-    ("%.02f" % (endy - start)).to_f # rounding, lazy decimal LOL
+    ("%.2f" % (endy - start)).to_f # rounding, lazy decimal LOL
   end
   
   def url
