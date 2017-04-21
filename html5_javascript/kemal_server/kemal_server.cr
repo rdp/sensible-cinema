6b83a479bd4a505c2844564a65642917a147cb34
rogerdpack <rogerpack2005@gmail.com>
2017-04-20 18:11:52 -0600

    moh email

--- html5_javascript/kemal_server/kemal_server.cr
+++ html5_javascript/kemal_server/kemal_server.cr
@@ -503,7 +503,8 @@ post "/send_me_mail" do |env|
   # https://askubuntu.com/a/13118/20972
   # TODO use a better email addy once it works/can work??
   system("sendemail -f freeldssheetmusic@gmail.com -t #{email} -u 'Link to the edited movie site' -m 'Here is the link! https://playitmyway.org see you soon!' -s smtp.gmail.com -o tls=yes -xu freeldssheetmusic@gmail.com -xp #{File.read("email_pass").strip} -s smtp.gmail.com:587")
-  "Success, you should see an email in your inbox now!"
+  add_to_flash env, "Success, send an invitation email to #{email}, you should see an email in your inbox now!"
+  env.redirect "/"
 end
 
 post "/save_tag_edit_list" do |env|
