e32c51df043f63e74841c761f21440a764db035b
rogerdpack <rogerpack2005@gmail.com>
2017-04-24 09:35:39 -0600

    thought uh did this

--- html5_javascript/kemal_server/sync_to_prod_with_commit.sh
+++ html5_javascript/kemal_server/sync_to_prod_with_commit.sh
@@ -1,4 +1,4 @@
-git pull && git cam "$1"  # this one can fail if none, that's OK
+git pull && (git cam "$1" || git cam "a commit")  # this one can fail if none, that's OK
 git pom && curl https://playitmyway.org/sync_web_server
 until $(curl --output /dev/null --silent --head --fail https://playitmyway.org); do
     printf '.'
