24676f2cbadbb606582d453c061c14bd4e6585fa
rogerdpack <rogerpack2005@gmail.com>
2017-04-25 16:27:19 -0600

    must have removed it, oh well

--- html5_javascript/kemal_server/kemal_server.cr
+++ html5_javascript/kemal_server/kemal_server.cr
@@ -514,10 +514,10 @@ get "/personalized_edit_list/:url_id" do |env|
   url_id = env.params.url["url_id"].to_i
   tag_edit_list = TagEditList.get_only_by_url_id_or_nil url_id, user_id(env)
   tag_edit_list ||= TagEditList.new url_id, user_id(env)
-    # and not save yet :|
+  # and not save yet :|
   show_tag_details =  env.params.query["show_tag_details"]?
     
-  render "views/list_edit_tag_list.ecr", "views/layout.ecr"
+  render "views/personalized_edit_list.ecr", "views/layout.ecr"
 end
 
 post "/send_me_mail" do |env|
