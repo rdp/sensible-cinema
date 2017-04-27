a7eb3e57c9c5898446e5f33e01e4b20dfea7fa6f
rogerdpack <rogerpack2005@gmail.com>
2017-04-26 22:06:08 -0600

    a commit

--- html5_javascript/kemal_server/sync_to_prod_with_commit.sh
+++ html5_javascript/kemal_server/sync_to_prod_with_commit.sh
@@ -1,7 +1,7 @@
 git pull && (git cam "$1" || git cam "a commit")  # this one can fail if none, that's OK
 git pom 
 echo "waiting for server to rebuild.."
-curl https://playitmyway.org/sync_web_server
+curl https://playitmyway.org/sync_web_server || exit 1
 until $(curl --output /dev/null --silent --head --fail https://playitmyway.org); do
     printf '.'
     sleep 1
