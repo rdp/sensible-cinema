2c31fed038d5f09d9939ef5fd6b013cd92d5dcb5
rogerdpack <rogerpack2005@gmail.com>
2017-05-01 17:18:41 -0600

    sanity input

--- html5_javascript/kemal_server/db/nuke_and_recreate_for_test.sh
+++ html5_javascript/kemal_server/db/nuke_and_recreate_for_test.sh
@@ -2,6 +2,8 @@
 
 mysql -uroot mysql < ./db/init.sql # read your creds from ./db/XXX if required
 
+rm sessions/* # in case they were logged in as a "now nuked" user
+
 # to *not* nuke one just run
 # mysql -uXXX -p db_name
 # and enter commands manually from bottom of init.sql
