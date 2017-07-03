c567f47659562602994622097f1089bede8305c6
rogerdpack <rogerpack2005@gmail.com>
2017-07-03 11:35:39 -0600

    better edit for textmate

--- html5_javascript/chrome_extension/generate_it.cr
+++ html5_javascript/chrome_extension/generate_it.cr
@@ -3,8 +3,6 @@ require "io/memory"
 require "../kemal_server/src/helpers/subcategories.cr" 
 require "html"
 
-# name = "World"
-
 io =  IO::Memory.new
-ECR.embed "edited_generic_player.js.ecr", io
+ECR.embed "edited_generic_player.ecr.js", io
 File.write("edited_generic_player.js", "//auto-generated file\n" + io.to_s)
