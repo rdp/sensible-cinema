392f248286d3ef7a2ed22b3c1dc10013e8209295
rogerdpack <rogerpack2005@gmail.com>
2017-07-19 16:42:39 -0600

    take3, plus out with the old

--- html5_javascript/chrome_extension/edited_generic_player.ecr.js
+++ html5_javascript/chrome_extension/edited_generic_player.ecr.js
@@ -1072,17 +1072,6 @@ var addEvent = function(object, type, callback) {
     }
 };
 
-function decodeHTMLEntities(text) {
- 	// I guess there's an HTML way to do this, but this way looked funner! :)
-  var entities = [
-      ['amp', '&'], ['apos', '\''], ['#x27', '\''], ['#x2F', '/'], ['#39', '\''], ['#47', '/'], ['lt', '<'], ['gt', '>'], ['nbsp', ' '], ['quot', '"']
-  ];
-  for (var i = 0, max = entities.length; i < max; ++i) {
-      text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
-  }
-  return text;
-}
-
 function displayDiv(div) {
 	div.style.display = "block";
 }
