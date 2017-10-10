15fd221a1046cc5bd924df2252aaf63b82f08782
rogerdpack <rogerpack2005@gmail.com>
2017-10-10 07:29:04 -0700

    nope, also note sooo close android

--- html5_javascript/chrome_extension/generate_it.cr
+++ html5_javascript/chrome_extension/generate_it.cr
@@ -8,4 +8,6 @@ io =  IO::Memory.new
 ECR.embed "edited_generic_player.ecr.js", io
 File.write("edited_generic_player.js", "//auto-generated file\n" + io.to_s)
 File.write("../kemal_server/public/plugin_javascript/edited_generic_player.js", "//auto-generated file\n" + io.to_s)
-FileUtils.cp("contentscript.js", "../kemal_server/public/plugin_javascript") # in case changed
+# can't easily use this
+# as it relies on relative loading the other?
+# FileUtils.cp("contentscript.js", "../kemal_server/public/plugin_javascript") # in case changed
