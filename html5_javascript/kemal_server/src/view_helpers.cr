74b22d7ae66f2f9e241094443e826eb00b7976d1
rogerdpack <rogerpack2005@gmail.com>
2017-10-09 22:23:06 -0700

    maybe

--- html5_javascript/kemal_server/src/view_helpers.cr
+++ html5_javascript/kemal_server/src/view_helpers.cr
@@ -21,7 +21,7 @@ end
 
 def is_mobile(env)
   ua = env.request.headers["User-Agent"]? 
-  ua =~ /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/ && (ua !~ PlayItMyWayUndroid)
+  ua =~ /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/ && (ua !~ /PlayItMyWayUndroid/)
 end
 
 def editor?(env)
