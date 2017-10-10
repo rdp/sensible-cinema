a259e3c96990d7255f00a826fe0381f17521c5a4
rogerdpack <rogerpack2005@gmail.com>
2017-10-09 21:21:08 -0700

    more

--- html5_javascript/AndroidApp/app/src/main/java/com/example/rdp/myfirstapplication/DisplayMessageActivity.java
+++ html5_javascript/AndroidApp/app/src/main/java/com/example/rdp/myfirstapplication/DisplayMessageActivity.java
@@ -30,7 +30,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         webSettings.setJavaScriptEnabled(true);
-        myWebView.getSettings().setDomStorageEnabled(true);
+       // myWebView.getSettings().setDomStorageEnabled(true);
         myWebView.loadUrl("https://playitmyway.org");
 
         myWebView.setWebViewClient(new WebViewClient() {
@@ -38,22 +38,34 @@ public class DisplayMessageActivity extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
 
+                view.evaluateJavascript("var x = (document.getElementById('replace_me').innerHTML = 'texthere3')", null);
                 StringBuilder sb = new StringBuilder();
                 sb.append("alert('hello inject');");
                 view.loadUrl("javascript:" + sb.toString());
-                view.loadUrl("javascript:" +
-                        "function() {document.getElementById('replace_me').innerHTML = 'texthere';}()");
-                view.evaluateJavascript("alert('hello inject')", null);
-                view.evaluateJavascript("document.getElementById('replace_me').innerHTML = 'texthere';", null);
-                view.evaluateJavascript("var x = (document.getElementById('replace_me').innerHTML = 'texthere')", null);
-                // works!
+                view.loadUrl(
+                        "javascript:document.getElementById('replace_me').innerHTML = 'texthere1'; nil");
+                //view.evaluateJavascript("document.getElementById('replace_me').innerHTML = 'texthere2'", null);
                 view.evaluateJavascript("(function() { alert('x'); " +
                         "return { var1: document.title, var2: \"variable2\", var3: (var y = document.getElementById('replace_me').innerHTML }; })();", new ValueCallback<String>() {
                     @Override
                     public void onReceiveValue(String s) {
-                        System.out.println("LogName" + s); // Prints: {"var1":"variable1","var2":"variable2"}
+                        System.out.println("LogName" + s);
                     }
                 });
+
+                // works! (doesn't alert of course)
+                view.evaluateJavascript("(function() { alert('x'); " +
+                        "return { var1: document.title, var2: \"variable2\"}; })();", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String s) {
+                        System.out.println("LogName" + s); // Prints: {"var1":"Play it My Way : Edited streaming movies","var2":"variable2"}
+                    }
+                });
+
+                // works
+                //view.loadUrl(
+                //        "javascript:(function()%7Bdocument.getElementById('replace_me').innerHTML%20%3D%20'texthere1'%7D)()");
+                // kind of works, with some weird unicode bytes in there LOL
                 view.evaluateJavascript(
                         "(function() { return ('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>'); })();",
                         new ValueCallback<String>() {
@@ -63,8 +75,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
                                 // code here
                             }
                         });
-
-
 
                 // actually worked!
                 //  view.loadUrl("javascript:document.write('hello')");
