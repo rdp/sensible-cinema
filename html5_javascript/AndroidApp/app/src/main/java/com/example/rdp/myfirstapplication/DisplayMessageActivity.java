68603e5d06bf6e27b7d0513304dd2553ff9f3ce2
rogerdpack <rogerpack2005@gmail.com>
2017-10-16 22:14:49 -0700

    Revert "one guys try"
    This reverts commit a606f625c828f71085b7fee00f7912ca21deb141.

--- html5_javascript/AndroidApp/app/src/main/java/com/example/rdp/myfirstapplication/DisplayMessageActivity.java
+++ html5_javascript/AndroidApp/app/src/main/java/com/example/rdp/myfirstapplication/DisplayMessageActivity.java
@@ -1,8 +1,6 @@
 package com.example.rdp.myfirstapplication;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -17,12 +15,103 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import static android.webkit.PermissionRequest.RESOURCE_PROTECTED_MEDIA_ID;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    private static final String TAG = "MyActivity";
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        final String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        myWebView = (WebView) findViewById(R.id.webView1);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setAllowContentAccess(true);
+        myWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setDisplayZoomControls(true);
+        myWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setUseWideViewPort(true);
+        
+        // Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.135 Safari/537.36 is chrome with "desktop" checked
+        myWebView.getSettings().setUserAgentString("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.135 Safari/537.36 PlayItMyWay/0.2");
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            // required seemingly for my cookies to connect with logged in user
+           CookieManager cookieManager = CookieManager.getInstance();
+           cookieManager.setAcceptThirdPartyCookies(myWebView, true);
+        }
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                request.grant(new String[]{RESOURCE_PROTECTED_MEDIA_ID});
+            }
+
+
+        });
+
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false; // default
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // loadUrl "might" be broken on real devices wait what?
+                Log.v(TAG, "url=" + url);
+                StringBuilder sb = new StringBuilder(); // XXXX I load this thrice??? huh wuh?
+                // XXXX more smarts here, also adjust UA or something? More instructions? Add link to your library?
+                // reload button [?] real fullscreen [?]
+                //
+                if (url.contains("amazon.com") || (url.contains("playitmyway.org") && !url.contains("youtube_pimw_edited"))) {
+                    sb.append("var my_awesome_script = document.createElement('script'); my_awesome_script.setAttribute('src','https://playitmyway.org/plugin_javascript/edited_generic_player.js'); document.head.appendChild(my_awesome_script);");
+                }
+
+                if (url.equals("https://playitmyway.org/")) {
+                    sb.append("document.getElementById('replace_me').innerHTML = 'Play it my way browser enabled!'; null;");
+                }
+                view.loadUrl("javascript:" + sb.toString());
+            }
+        });
+
+        if (message.startsWith("http")) {
+            myWebView.loadUrl(message);
+        } else {
+            myWebView.loadUrl("https://playitmyway.org");
+        }
+    }
+
+    @Override
+    public void onBackPressed() { // allow android button to navigate within webview
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myWebView.destroy(); // seems necessary. What?
+    }
 }
