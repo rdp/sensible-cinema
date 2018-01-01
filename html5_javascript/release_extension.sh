c8f0a716d64d14051c3b12a3310648cd01ecbfe4
rogerdpack <rogerpack2005@gmail.com>
2018-01-01 12:02:58 -0700

    notes

--- html5_javascript/release_extension.sh
+++ html5_javascript/release_extension.sh
@@ -1,4 +1,5 @@
-echo "this doesn't rebuild the extension...fwiw...or bump the version"
+echo "this doesn't rebuild the extension...fwiw...or bump the version...nor re-deploy to server"
 rm -f *.zip
 zip -r chrome_extension_release.zip chrome_extension
 git commit -am "release extension..."
+git pom
