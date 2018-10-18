66c751fd0a6cb431045c3aec8a13457d456fb12d
rogerdpack <rogerpack2005@gmail.com>
2018-10-17 23:19:54 -0600

    debug

--- html5_javascript/kemal_server/kemal_server.cr
+++ html5_javascript/kemal_server/kemal_server.cr
@@ -684,8 +684,11 @@ def get_in_works(all_urls)
   all_urls_just_started = all_urls.select{|url| url.edit_passes_completed == 0 }
   in_works = all_urls_half_way + all_urls_just_started
   in_works.sort_by!{|u| u.name} # combine lists
+  puts "after name=", in_works.map &.name
   in_works.sort_by!{|u| (u.name =~ /game of thrones/i) != nil} # put last there's soo many
+  puts "after got=", in_works.map &.name
   put_test_last(in_works) # not reallly for human consumption in lists...
+  puts "after test last=", in_works.map &.name
   return in_works
 end
 
