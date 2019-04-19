4427b8bd0999b3442894ae3fc8451fc91b3fdaac
rogerdpack <rogerpack2005@gmail.com>
2019-04-18 20:05:35 -0400

    fix link

--- html5_javascript/kemal_server/src/straight_forward.cr
+++ html5_javascript/kemal_server/src/straight_forward.cr
@@ -2,6 +2,10 @@ get "/installation" do |env|
   render "views/installation.ecr", "views/layout_yes_nav.ecr"
 end
 
+get "/supporter" do |env|
+  render "views/supporter.ecr", "views/layout_nik.ecr"
+end
+
 get "/jobs" do |env|
   render "views/jobs.ecr", "views/layout_nik.ecr"
 end
