613f1076f40c6ffff8ce0a407facc62e6eaeb7b4
rdp <rogerpack2005@gmail.com>
2019-07-27 16:38:26 -0600

    a commit

--- html5_javascript/kemal_server/sync_to_prod_with_commit.sh
+++ html5_javascript/kemal_server/sync_to_prod_with_commit.sh
@@ -4,7 +4,7 @@ cd ../chrome_extension
 popd
 git pull && (git cam "$1" || git cam "a commit")  # this one can fail if none, that's OK
 git pom 
-echo "waiting for server to rebuild.."
+echo "instructing server to start rebuild..."
 curl https://playitmyway.org/sync_web_server || exit 1
 echo "waiting for restart"
 until $(curl --output /dev/null --silent --head --fail https://playitmyway.org); do
