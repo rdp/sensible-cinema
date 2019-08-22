9449cf58f0fc240c196e0facc9b16423d2001f76
rdp <rogerpack2005@gmail.com>
2019-08-22 17:41:36 -0600

    revert

--- html5_javascript/kemal_server/bad.cr
+++
@@ -1,6 +0,0 @@
-require "socket"
-TCPSocket.open("localhost", 8081) { |socket|
-  puts "connected #{socket}"
-  sleep
-}
-
--- html5_javascript/kemal_server/openssl2.cr
+++
@@ -1,9 +0,0 @@
-require "http/server"
-
-server = HTTP::Server.new { }
-context = OpenSSL::SSL::Context::Server.new
-context.certificate_chain = "cert.pem"
-context.private_key = "_key.pem"
-address = server.bind_tls Socket::IPAddress.new("127.0.0.1", 8000), context
-puts "listening https://127.0.0.1:8000"
-server.listen
