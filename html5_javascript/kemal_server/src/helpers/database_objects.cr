4d146c290ffdc58cc550b9e8c28e02ba9194e80b
rogerdpack <rogerpack2005@gmail.com>
2019-10-11 01:44:56 -0400

    me

--- html5_javascript/kemal_server/src/helpers/database_objects.cr
+++ html5_javascript/kemal_server/src/helpers/database_objects.cr
@@ -152,10 +152,9 @@ class Url
 
   def self.get_only_or_nil_by_loose_amazon_search(url, episode_number)
     # url is like https:&#x2F;&#x2F;www.amazon.com&#x2F;Ex-Machina-Alicia-Vikander&#x2F;dp/B011KKCQH8
-    puts "name=#{url}"
     name = url.split("&#x2F;")[3]
     name = "%#{name}%"
-    # TODO this is a tidge lame tho??/
+    # XXXX this is a tidge lame tho???
     urls = query("SELECT * FROM urls WHERE (url like ? or amazon_second_url like ? or amazon_third_url like ?) AND episode_number = ?", name, name, name, episode_number) do |rs|
        Url.from_rs(rs);
     end
