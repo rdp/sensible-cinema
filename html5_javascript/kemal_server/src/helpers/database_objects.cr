841d61cd01f05d5662a83c66a53b687b9ba95d59
rdp <rogerpack2005@gmail.com>
2020-01-01 10:30:03 -0700

    a commit

--- html5_javascript/kemal_server/src/helpers/database_objects.cr
+++ html5_javascript/kemal_server/src/helpers/database_objects.cr
@@ -259,7 +259,7 @@ class Url
   end
 
   def url_lookup_params
-    "url=#{url}&episode_number=#{episode_number}" # URI.escape?
+    "url=#{url}&episode_number=#{episode_number}" # URI.encode?
   end
 
   def human_duration
